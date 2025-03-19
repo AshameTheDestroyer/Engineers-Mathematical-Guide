1cb4a780a8117a14278581fcc4a89161896b8344
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-19 13:40:07 +0300

    Paths Reverted Temporarily.

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
-import { Page } from "@components/Page/Page";
 import { Header } from "./components/Header";
+import { Page } from "../../components/Page/Page";
 import { MainContent } from "./components/MainContent";
 
 export const LandingPage: FC = () => {
