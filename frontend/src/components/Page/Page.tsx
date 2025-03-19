fdc5ef81ab13384c979d3513f478c358b9218cfa
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-19 12:27:03 +0300

    Page Spacing Variable.

--- frontend/src/components/Page/Page.tsx
+++ frontend/src/components/Page/Page.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
+import { twMerge } from "tailwind-merge";
 import { ComponentProps } from "../../types/ComponentProps";
 
 export const Page: FC<ComponentProps> = ({ id, className, children }) => {
     return (
         <main
             id={id}
-            className={[
-                "page flex min-h-screen flex-col",
-                className,
-            ].toClassName()}
+            className={twMerge(
+                "page p-page flex min-h-screen flex-col",
+                className
+            )}
         >
             {children}
         </main>
