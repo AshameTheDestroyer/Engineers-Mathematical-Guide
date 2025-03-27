edcd932d4faf2c5bb4f7c5649b6f43c071add307
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-28 01:59:00 +0300

    Cogs in Background.

--- frontend/src/pages/LandingPage/components/MainContent.tsx
+++ frontend/src/pages/LandingPage/components/MainContent.tsx
@@ -1,8 +1,10 @@
 import { FC } from "react";
+import { Icon } from "@components/Icon/Icon";
 import { Button } from "@components/Button/Button";
 import { ButtonBox } from "@components/ButtonBox/ButtonBox";
 
 import pi_image from "@images/pi.png";
+import cog_icon from "@icons/cog.svg";
 import arrow_icon from "@icons/arrow.svg";
 
 // export const MainContent: FC = () => {
@@ -78,6 +80,24 @@ export const MainContent: FC = () => {
                 className="w-100 [filter:drop-shadow(4px_4px_2px_#0000007c)]"
                 src={pi_image}
                 alt="pi image"
+            />
+            <Icon
+                className="text-background-dark -left-page absolute bottom-0 z-[-1] -translate-x-1/4 translate-y-1/4 animate-spin [animation-duration:60s]"
+                width={400}
+                height={400}
+                source={cog_icon}
+            />
+            <Icon
+                className="text-background-dark -left-page absolute bottom-0 z-[-1] translate-x-[70%] translate-y-[40%] rotate-[-5deg] animate-spin [animation-direction:reverse] [animation-duration:60s]"
+                width={350}
+                height={350}
+                source={cog_icon}
+            />
+            <Icon
+                className="text-background-dark -right-page absolute top-0 translate-x-1/4 translate-y-2/3 animate-spin [animation-direction:reverse] [animation-duration:60s]"
+                width={250}
+                height={250}
+                source={cog_icon}
             />
         </div>
     );
