7280b9ef3e51d68ee2642219ea18381ec80a0c89
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-28 03:26:30 +0300

    Cogs in Not Found Page.

--- frontend/src/pages/NotFoundPage/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 import { Page } from "@components/Page/Page";
+import { Icon } from "@components/Icon/Icon";
 import { Button } from "@components/Button/Button";
 
+import cog_icon from "@icons/cog.svg";
 import home_icon from "@icons/home.svg";
 
 export const NotFoundPage: FC = () => {
     return (
-        <Page className="bg-primary-normal text-white">
-            <div className="centralize bg-gray flex h-32 grow flex-col justify-between gap-5">
+        <Page className="bg-primary-normal overflow-hidden text-white">
+            <div className="centralize bg-gray isolate flex h-32 grow flex-col justify-between gap-5 overflow-hidden">
                 <h1 className="text-3xl font-bold">Page Not Found</h1>
                 <p>
                     Please check your route carefully, because the URL you
@@ -23,6 +25,30 @@ export const NotFoundPage: FC = () => {
                 >
                     Return Home
                 </Button>
+                <Icon
+                    className="-left-page absolute bottom-0 z-[-1] -translate-x-1/4 translate-y-1/4 animate-spin text-white [animation-duration:60s]"
+                    width={400}
+                    height={400}
+                    source={cog_icon}
+                />
+                <Icon
+                    className="-left-page absolute bottom-0 z-[-1] translate-x-[70%] translate-y-[40%] rotate-[-5deg] animate-spin text-white [animation-direction:reverse] [animation-duration:60s]"
+                    width={350}
+                    height={350}
+                    source={cog_icon}
+                />
+                <Icon
+                    className="-right-page absolute top-0 translate-x-1/4 translate-y-2/3 animate-spin text-white [animation-direction:reverse] [animation-duration:60s]"
+                    width={250}
+                    height={250}
+                    source={cog_icon}
+                />
+                <Icon
+                    className="-left-page absolute top-0 -translate-y-1/2 translate-x-[10vw] animate-spin text-white [animation-direction:reverse] [animation-duration:60s]"
+                    width={250}
+                    height={250}
+                    source={cog_icon}
+                />
             </div>
         </Page>
     );
