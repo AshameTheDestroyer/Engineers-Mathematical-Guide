51885768a47131f4938671d94698d6132d73dda8
Batoul Kazak <batoulkazak297@gmail.com>
2025-03-28 22:47:11 +0300

    m

--- frontend/src/components/Checkbox/Checkbox.tsx
+++ frontend/src/components/Checkbox/Checkbox.tsx
@@ -18,7 +18,7 @@ const Checkbox: React.FC<CustomCheckboxProps> = ({
     label,
     link,
     linkText,
-    variant,
+    variant = "default",
 }) => {
     const [isChecked, setIsChecked] = useState(false);
 
@@ -30,36 +30,10 @@ const Checkbox: React.FC<CustomCheckboxProps> = ({
         }
     };
 
-    const variantClassName = (() => {
-        let className: string = "";
-        switch (variant) {
-            case "default":
-                return (className = `
-                        [&>[data-content]]:bg-white [&>*]:border-gray-normal [&>[data-thickness]]:bg-gray-normal text-foreground-light
-                        [&:where(&:hover,&:focus-within)]:[&>[data-content]]:bg-gray-normal [&:where(&:hover,&:focus-within)]:text-foreground-light-hover [&:where(&:hover,&:focus-within)]:[&>[data-thickness]]:bg-gray-normal-hover [&:where(&:hover,&:focus-within)]:[&>*]:border-gray-normal-hover
-                        active:[&>[data-content]]:bg-gray-normal active:text-foreground-light-active active:[&>[data-thickness]]:bg-gray-normal-active active:[&>*]:border-gray-normal-active
-                    `);
-            case "primary":
-                return (className = `
-                        [&>[data-content]]:bg-primary-normal [&>*]:border-primary-dark text-primary-light font-bold [&>[data-thickness]]:bg-primary-dark
-                        [&:where(&:hover,&:focus-within)]:[&>[data-content]]:bg-primary-normal-hover [&:where(&:hover,&:focus-within)]:text-primary-light-hover [&:where(&:hover,&:focus-within)]:[&>[data-thickness]]:bg-primary-dark-hover [&:where(&:hover,&:focus-within)]:[&>*]:border-primary-dark-hover
-                        active:[&>[data-content]]:bg-primary-normal-active active:text-primary-light-active active:[&>[data-thickness]]:bg-primary-dark-active active:[&>*]:border-primary-dark-active
-                    `);
-            case "secondary":
-                return (className = `
-                        [&>[data-content]]:bg-secondary-normal [&>*]:border-secondary-dark text-secondary-light font-bold [&>[data-thickness]]:bg-secondary-dark
-                        [&:where(&:hover,&:focus-within)]:[&>[data-content]]:bg-secondary-normal-hover [&:where(&:hover,&:focus-within)]:text-secondary-light-hover [&:where(&:hover,&:focus-within)]:[&>[data-thickness]]:bg-secondary-dark-hover [&:where(&:hover,&:focus-within)]:[&>*]:border-secondary-dark-hover
-                        active:[&>[data-content]]:bg-secondary-normal-active active:text-secondary-light-active active:[&>[data-thickness]]:bg-secondary-dark-active active:[&>*]:border-secondary-dark-active
-                    `);
-            default:
-                return className;
-        }
-    })();
-
     return (
         <label className="flex cursor-pointer items-center">
             <div
-                className={`rounded-1xl border-primary-dark flex h-6 w-6 items-center justify-center border-2 transition-colors duration-300 ${variant} ${className} ${
+                className={`rounded-1xl border-primary-dark flex h-6 w-6 items-center justify-center border-2 transition-colors duration-300 ${className} ${
                     isChecked
                         ? "bg-primary-normal border-transparent"
                         : "bg-white"
--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -9,7 +9,7 @@ interface CustomInputProps {
     className?: string;
     error?: string;
     label: string;
-    variant?: string;
+    variant: string;
 }
 
 const Input: React.FC<CustomInputProps> = ({
@@ -22,32 +22,6 @@ const Input: React.FC<CustomInputProps> = ({
     label,
     variant = "default",
 }) => {
-    const variantClassName = (() => {
-        let className: string = "";
-        switch (variant) {
-            case "default":
-                return (className = `
-                        [&>[data-content]]:bg-white [&>*]:border-gray-normal [&>[data-thickness]]:bg-gray-normal text-foreground-light
-                        [&:where(&:hover,&:focus-within)]:[&>[data-content]]:bg-gray-normal [&:where(&:hover,&:focus-within)]:text-foreground-light-hover [&:where(&:hover,&:focus-within)]:[&>[data-thickness]]:bg-gray-normal-hover [&:where(&:hover,&:focus-within)]:[&>*]:border-gray-normal-hover
-                        active:[&>[data-content]]:bg-gray-normal active:text-foreground-light-active active:[&>[data-thickness]]:bg-gray-normal-active active:[&>*]:border-gray-normal-active
-                    `);
-            case "primary":
-                return (className = `
-                        [&>[data-content]]:bg-primary-normal [&>*]:border-primary-dark text-primary-light font-bold [&>[data-thickness]]:bg-primary-dark
-                        [&:where(&:hover,&:focus-within)]:[&>[data-content]]:bg-primary-normal-hover [&:where(&:hover,&:focus-within)]:text-primary-light-hover [&:where(&:hover,&:focus-within)]:[&>[data-thickness]]:bg-primary-dark-hover [&:where(&:hover,&:focus-within)]:[&>*]:border-primary-dark-hover
-                        active:[&>[data-content]]:bg-primary-normal-active active:text-primary-light-active active:[&>[data-thickness]]:bg-primary-dark-active active:[&>*]:border-primary-dark-active
-                    `);
-            case "secondary":
-                return (className = `
-                        [&>[data-content]]:bg-secondary-normal [&>*]:border-secondary-dark text-secondary-light font-bold [&>[data-thickness]]:bg-secondary-dark
-                        [&:where(&:hover,&:focus-within)]:[&>[data-content]]:bg-secondary-normal-hover [&:where(&:hover,&:focus-within)]:text-secondary-light-hover [&:where(&:hover,&:focus-within)]:[&>[data-thickness]]:bg-secondary-dark-hover [&:where(&:hover,&:focus-within)]:[&>*]:border-secondary-dark-hover
-                        active:[&>[data-content]]:bg-secondary-normal-active active:text-secondary-light-active active:[&>[data-thickness]]:bg-secondary-dark-active active:[&>*]:border-secondary-dark-active
-                    `);
-            default:
-                return className;
-        }
-    })();
-
     return (
         <div className="mb-4">
             <label className="bg-primary-dark-active text-primary-light ml-10 rounded-3xl p-2 shadow-lg">
