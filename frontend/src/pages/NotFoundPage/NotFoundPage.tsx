dc1c2de4539d7bb59a2cbc64c459ef63c2c32159
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-28 03:13:40 +0300

    Return Home.

--- frontend/src/pages/NotFoundPage/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,8 @@
 import { FC } from "react";
 import { Page } from "@components/Page/Page";
+import { Button } from "@components/Button/Button";
+
+import home_icon from "@icons/home.svg";
 
 export const NotFoundPage: FC = () => {
     return (
@@ -10,6 +13,16 @@ export const NotFoundPage: FC = () => {
                     Please check your route carefully, because the URL you
                     provided does not exist.
                 </p>
+                <Button
+                    className="[&>div]:px-8"
+                    link="/"
+                    icon={{
+                        source: home_icon,
+                        placement: "left",
+                    }}
+                >
+                    Return Home
+                </Button>
             </div>
         </Page>
     );
