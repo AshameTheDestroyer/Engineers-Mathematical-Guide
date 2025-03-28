99a8a5abfe6a513a52915e85ab709ae25827b042
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-28 03:00:23 +0300

    Header Fades on Scroll.

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,15 +1,46 @@
-import { FC } from "react";
 import { twMerge } from "tailwind-merge";
+import { FC, RefObject, useEffect } from "react";
 import { ComponentProps } from "@types_/ComponentProps";
 
-export type HeaderProps = ComponentProps;
+export type HeaderProps = ComponentProps & {
+    isSticky?: boolean;
+    ref?: RefObject<HTMLDivElement>;
+    onScroll?: (rect: Record<"top" | "left", number>) => void;
+};
 
-export const Header: FC<HeaderProps> = ({ id, children, className }) => {
+export const Header: FC<HeaderProps> = ({
+    id,
+    ref,
+    children,
+    isSticky,
+    onScroll,
+    className,
+}) => {
+    useEffect(() => {
+        if (onScroll == null) {
+            return;
+        }
+
+        const scrollCallback = () =>
+            onScroll({
+                top: window.scrollY,
+                left: window.scrollX,
+            });
+
+        window.addEventListener("scroll", scrollCallback);
+
+        return () => {
+            window.removeEventListener("scroll", scrollCallback);
+        };
+    }, [onScroll]);
+
     return (
         <header
             id={id}
+            ref={ref}
             className={twMerge(
-                "-m-page mb-page px-page sticky top-0 z-10 flex place-items-center justify-between gap-8 py-[calc(var(--spacing-page)/2)]",
+                isSticky ? "sticky top-0" : "",
+                "-m-page mb-page px-page z-10 flex place-items-center justify-between gap-8 py-[calc(var(--spacing-page)/2)]",
                 className
             )}
         >
--- frontend/src/pages/LandingPage/components/Header.tsx
+++ frontend/src/pages/LandingPage/components/Header.tsx
@@ -1,17 +1,38 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import { Logo } from "@components/Logo/Logo";
 import { Button } from "@components/Button/Button";
 import { HEADER_ROUTES } from "@constants/HeaderRoutes";
 import { ButtonBox } from "@components/ButtonBox/ButtonBox";
-import { Header as Header_ } from "@components/Header/Header";
 import { NavigationBar } from "@components/NavigationBar/NavigationBar";
+import { Header as Header_, HeaderProps } from "@components/Header/Header";
 
 import login_icon from "@icons/login.svg";
 import signup_icon from "@icons/user.svg";
 
 export const Header: FC = () => {
+    const headerReference = useRef<HTMLDivElement>(null!);
+
+    const onScroll: HeaderProps["onScroll"] = ({ top }) => {
+        if (headerReference.current == null) {
+            return;
+        }
+
+        const className = "not-hover:not-focus-within:opacity-50";
+
+        if (top > headerReference.current.clientHeight) {
+            headerReference.current.classList.add(className);
+        } else {
+            headerReference.current.classList.remove(className);
+        }
+    };
+
     return (
-        <Header_ className="bg-background-normal">
+        <Header_
+            ref={headerReference}
+            className="bg-background-normal transition duration-300"
+            isSticky
+            onScroll={onScroll}
+        >
             <Logo />
             <NavigationBar routes={HEADER_ROUTES} className="grow" />
             <ButtonBox>
