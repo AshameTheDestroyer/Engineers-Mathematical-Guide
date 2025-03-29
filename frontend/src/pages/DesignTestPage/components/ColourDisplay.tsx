0820b985664681dc2ff4965433ae56aecb080927
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-29 21:49:59 +0300

    Copyable Colours.

--- frontend/src/pages/DesignTestPage/components/ColourDisplay.tsx
+++ frontend/src/pages/DesignTestPage/components/ColourDisplay.tsx
@@ -46,6 +46,7 @@ export const ColourDisplay: FC<ColourDisplayProps> = ({ id, className }) => {
         <button
             id={id}
             ref={divReference}
+            title="Click to copy colour."
             className={twMerge(
                 "not-active:[:is(:hover,:focus-within)]:scale-110 flex cursor-pointer flex-col gap-2 rounded-2xl px-6 py-4 text-center font-bold transition-[scale] duration-200",
                 colourRGB != null && IsDarkColour(colourRGB)
@@ -53,6 +54,10 @@ export const ColourDisplay: FC<ColourDisplayProps> = ({ id, className }) => {
                     : "text-black",
                 className
             )}
+            onClick={(_e) =>
+                colourHex != null &&
+                navigator.clipboard.writeText(colourHex?.toLowerCase())
+            }
         >
             <h3 className="grow place-content-center">{colourName}</h3>
             <p className="text-xl">{colourHex != null && colourHex}</p>
