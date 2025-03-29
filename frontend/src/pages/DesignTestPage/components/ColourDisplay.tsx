1278e5aef52c6d34b4b029da2a2da6b63b21a889
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-29 18:22:01 +0300

    Colour Display Component.

---
+++ frontend/src/pages/DesignTestPage/components/ColourDisplay.tsx
@@ -0,0 +1,63 @@
+import { twMerge } from "tailwind-merge";
+import { ChildlessComponentProps } from "@/types/ComponentProps";
+import { FC, useEffect, useMemo, useRef, useState } from "react";
+
+export type ColourDisplayProps = ChildlessComponentProps;
+
+export const ColourDisplay: FC<ColourDisplayProps> = ({ id, className }) => {
+    const divReference = useRef<HTMLButtonElement>(null);
+    const [colourValue, setColourValue] = useState("");
+
+    const colourName = useMemo(
+        () =>
+            className
+                ?.trimAll()
+                .split(" ")
+                .find((token) => token.startsWith("bg-"))
+                ?.replace("bg-", "")
+                .replaceAll("-", " ")
+                .toTitleCase(),
+        [className]
+    );
+
+    useEffect(() => {
+        if (divReference.current == null) {
+            return;
+        }
+
+        setColourValue(
+            RGBToHex(
+                divReference.current
+                    .computedStyleMap()
+                    .get("background-color")!
+                    .toString()
+            )
+        );
+    }, []);
+
+    function RGBToHex(text: string) {
+        return (
+            "#" +
+            text
+                .replace("rgb(", "")
+                .replace(")", "")
+                .split(", ")
+                .map((c) => Number(c).toString(16).toUpperCase())
+                .join("")
+        );
+    }
+
+    return (
+        <button
+            id={id}
+            ref={divReference}
+            className={twMerge(
+                "not-active:[:is(:hover,:focus-within)]:scale-110 cursor-pointer rounded-2xl px-6 py-4 text-center font-bold transition-[scale] duration-200",
+                className
+            )}
+        >
+            <h1>{colourName}</h1>
+            <p>{colourValue}</p>
+        </button>
+    );
+};
