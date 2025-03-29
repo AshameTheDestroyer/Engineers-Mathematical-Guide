e7e26770e17d508e4c4bcfa8a83dca44fe0acb98
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-29 21:34:47 +0300

    Assert Colour Is Utility Function.

--- frontend/src/functions/HexToRGB.ts
+++ frontend/src/functions/HexToRGB.ts
@@ -1,23 +1,18 @@
-export function HexToRGB(text: Hex): RGB | RGBA {
-    if (
-        isNaN(parseInt(text.replace("#", ""), 16)) ||
-        !text.startsWith("#") ||
-        text.length < 7 ||
-        text.length > 9
-    ) {
-        throw new Error("Text wasn't in the correct format.");
-    }
+import { AssertColourIsHex } from "./AssertColourIs";
 
-    if (text.length >= 7) {
-        const r = parseInt(text.slice(1, 3), 16);
-        const g = parseInt(text.slice(3, 5), 16);
-        const b = parseInt(text.slice(5, 7), 16);
+export function HexToRGB(colour: Hex): RGB | RGBA {
+    AssertColourIsHex(colour);
 
-        if (text.length == 7) {
+    if (colour.length >= 7) {
+        const r = parseInt(colour.slice(1, 3), 16);
+        const g = parseInt(colour.slice(3, 5), 16);
+        const b = parseInt(colour.slice(5, 7), 16);
+
+        if (colour.length == 7) {
             return `rgb(${r}, ${g}, ${b})` as RGB;
-        } else if (text.length == 9) {
+        } else if (colour.length == 9) {
             const a = Number(
-                (parseInt(text.slice(7, 9), 16) / 255).toPrecision(2)
+                (parseInt(colour.slice(7, 9), 16) / 255).toPrecision(2)
             );
             return `rgb(${r}, ${g}, ${b}, ${a})` as RGBA;
         }
