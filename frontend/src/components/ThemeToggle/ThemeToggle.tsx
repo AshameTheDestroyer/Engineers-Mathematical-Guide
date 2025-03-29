ed17f16e33b2b9643ed9607b5fe2f68a6376f1d4
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-29 18:44:18 +0300

    Theme Toggle Props Added.

---
+++ frontend/src/components/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,32 @@
+import { MainContext } from "@/index";
+import { FC, useContext } from "react";
+import { IconButton, IconButtonProps } from "../IconButton/IconButton";
+
+import light_theme_icon from "@icons/sun.svg";
+import dark_theme_icon from "@icons/moon.svg";
+
+export type ThemeToggleProps = Omit<IconButtonProps, "icon">;
+
+export const ThemeToggle: FC<ThemeToggleProps> = ({
+    id,
+    className,
+    onClick,
+    ...props
+}) => {
+    const { isDarkThemed, ToggleDarkTheme } = useContext(MainContext);
+
+    return (
+        <IconButton
+            id={id}
+            className={className}
+            variant="secondary"
+            icon={{
+                width: 24,
+                height: 24,
+                source: isDarkThemed ? light_theme_icon : dark_theme_icon,
+            }}
+            onClick={(e) => (ToggleDarkTheme(), onClick?.(e))}
+            {...props}
+        />
+    );
+};
