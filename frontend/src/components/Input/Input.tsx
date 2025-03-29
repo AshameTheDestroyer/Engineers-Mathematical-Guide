dae4de4ecf9e2913d3559ef5a9dc67017fe8c403
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-30 02:18:47 +0300

    Better Organized.

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -10,10 +10,10 @@ export type CustomInputProps = {
     name: string;
     label: string;
     error?: string;
+    value?: string;
     variant?: Variant;
     className?: string;
     placeholder?: string;
-    value?: string | number;
     onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
@@ -63,12 +63,12 @@ const Input: React.FC<CustomInputProps> = ({
     label,
     onChange,
     className,
+    value: value_,
     variant = "default",
-    value: externalValue,
 }) => {
+    const [value, setValue] = useState<string>("");
     const [isFocused, setIsFocused] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
-    const [internalValue, setInternalValue] = useState<string | number>("");
 
     const handleFocus = () => setIsFocused(true);
     const handleBlur = () => setIsFocused(false);
@@ -77,22 +77,17 @@ const Input: React.FC<CustomInputProps> = ({
         if (onChange) {
             onChange(event);
         }
-        setInternalValue(event.target.value);
+        setValue(event.target.value);
     };
 
-    const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
-    };
-
-    const isLabelUp =
-        isFocused || (externalValue || internalValue).toString().trim() !== "";
-    const variantStyle = variantClasses[variant];
+    const variantClass = variantClasses[variant];
+    const isLabelUp = isFocused || (value_ ?? value).trimAll() !== "";
 
     const getLabelColor = () => {
-        if (error) return variantStyle.label.error;
-        if (isFocused) return variantStyle.label.focused;
-        if (isLabelUp) return variantStyle.label.filled;
-        return variantStyle.label.normal;
+        if (error) return variantClass.label.error;
+        if (isLabelUp) return variantClass.label.filled;
+        if (isFocused) return variantClass.label.focused;
+        return variantClass.label.normal;
     };
 
     const inputType = type === "password" && showPassword ? "text" : type;
@@ -100,7 +95,7 @@ const Input: React.FC<CustomInputProps> = ({
     return (
         <div className="relative mb-4">
             <label
-                className={twMerge(
+                className={twJoin(
                     getLabelColor(),
                     "absolute left-4 transition-all duration-300",
                     isLabelUp ? "-top-6 text-base" : "top-1/2 -translate-y-1/2"
@@ -111,14 +106,14 @@ const Input: React.FC<CustomInputProps> = ({
             <div className="relative">
                 <input
                     className={twMerge(
-                        variantStyle.focus,
-                        variantStyle.hover,
-                        variantStyle.border,
+                        variantClass.focus,
+                        variantClass.hover,
+                        variantClass.border,
                         type === "password" ? "pr-10" : "",
                         "peer block w-full rounded-md border px-4 py-2 focus:ring-2",
                         error
                             ? "border-red-500 text-red-800 outline-red-800"
-                            : variantStyle.border + " " + variantStyle.text,
+                            : variantClass.border + " " + variantClass.text,
                         className
                     )}
                     name={name}
@@ -126,19 +121,22 @@ const Input: React.FC<CustomInputProps> = ({
                     onBlur={handleBlur}
                     onFocus={handleFocus}
                     onChange={handleChange}
-                    value={externalValue ?? internalValue}
+                    value={value_ ?? value}
                 />
                 {type === "password" && (
                     <button
                         className={twJoin(
-                            variantStyle.text,
+                            variantClass.text,
                             "absolute right-3 top-1/2 -translate-y-1/2"
                         )}
                         type="button"
-                        onClick={togglePasswordVisibility}
-                        aria-label={
-                            showPassword ? "Hide password" : "Show password"
+                        title={
+                            showPassword ? "Hide password." : "Show password."
                         }
+                        aria-label={
+                            showPassword ? "Hide password." : "Show password."
+                        }
+                        onClick={(_e) => setShowPassword(!showPassword)}
                     >
                         <Icon source={showPassword ? eye_closed : eye_open} />
                     </button>
