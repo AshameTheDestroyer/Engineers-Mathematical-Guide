9af2dfdb43b1a548cf5157041a585910550d3c8d
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-30 06:13:28 +0300

    Better Icon-ed.

--- frontend/src/pages/TestPage/components/ButtonCollection.tsx
+++ frontend/src/pages/TestPage/components/ButtonCollection.tsx
@@ -1,12 +1,32 @@
 import { FC } from "react";
-import { Button } from "@/components/Button/Button";
 import { Collection } from "../components/Collection";
 import { ButtonBox } from "@/components/ButtonBox/ButtonBox";
+import { Button, ButtonProps } from "@/components/Button/Button";
 
 import home_icon from "@icons/home.svg";
 import arrow_icon from "@icons/arrow.svg";
 
 export const ButtonCollection: FC = () => {
+    const arrowRightIcon: ButtonProps["icon"] = {
+        width: 24,
+        source: arrow_icon,
+        placement: "right",
+        className: "rotate-90",
+    };
+
+    const arrowLeftIcon: ButtonProps["icon"] = {
+        width: 24,
+        source: arrow_icon,
+        placement: "left",
+        className: "-rotate-90",
+    };
+
+    const homeIcon: ButtonProps["icon"] = {
+        width: 24,
+        source: home_icon,
+        placement: "left",
+    };
+
     return (
         <Collection title="Button Component">
             <Collection title="Normal" inner>
@@ -31,109 +51,37 @@ export const ButtonCollection: FC = () => {
             </Collection>
             <Collection title="Icon-ed" inner className="[&>div]:flex-col">
                 <ButtonBox>
-                    <Button
-                        variant="default"
-                        icon={{
-                            width: 24,
-                            source: arrow_icon,
-                            placement: "right",
-                            className: "rotate-90",
-                        }}
-                    >
+                    <Button variant="default" icon={arrowRightIcon}>
                         Default
                     </Button>
-                    <Button
-                        variant="primary"
-                        icon={{
-                            width: 24,
-                            source: arrow_icon,
-                            placement: "right",
-                            className: "rotate-90",
-                        }}
-                    >
+                    <Button variant="primary" icon={arrowRightIcon}>
                         Primary
                     </Button>
-                    <Button
-                        variant="secondary"
-                        icon={{
-                            width: 24,
-                            source: arrow_icon,
-                            placement: "right",
-                            className: "rotate-90",
-                        }}
-                    >
+                    <Button variant="secondary" icon={arrowRightIcon}>
                         Secondary
                     </Button>
                 </ButtonBox>
                 <ButtonBox>
-                    <Button
-                        variant="default"
-                        icon={{
-                            width: 24,
-                            source: arrow_icon,
-                            placement: "left",
-                            className: "-rotate-90",
-                        }}
-                    >
+                    <Button variant="default" icon={arrowLeftIcon}>
                         Default
                     </Button>
-                    <Button
-                        variant="primary"
-                        icon={{
-                            width: 24,
-                            source: arrow_icon,
-                            placement: "left",
-                            className: "-rotate-90",
-                        }}
-                    >
+                    <Button variant="primary" icon={arrowLeftIcon}>
                         Primary
                     </Button>
-                    <Button
-                        variant="secondary"
-                        icon={{
-                            width: 24,
-                            source: arrow_icon,
-                            placement: "left",
-                            className: "-rotate-90",
-                        }}
-                    >
+                    <Button variant="secondary" icon={arrowLeftIcon}>
                         Secondary
                     </Button>
                 </ButtonBox>
             </Collection>
             <Collection title="Linked" inner>
                 <ButtonBox>
-                    <Button
-                        variant="default"
-                        link="/"
-                        icon={{
-                            width: 24,
-                            source: home_icon,
-                            placement: "left",
-                        }}
-                    >
+                    <Button variant="default" link="/" icon={homeIcon}>
                         Default
                     </Button>
-                    <Button
-                        variant="primary"
-                        link="/"
-                        icon={{
-                            width: 24,
-                            source: home_icon,
-                            placement: "left",
-                        }}
-                    >
+                    <Button variant="primary" link="/" icon={homeIcon}>
                         Primary
                     </Button>
-                    <Button
-                        variant="secondary"
-                        link="/"
-                        icon={{
-                            width: 24,
-                            source: home_icon,
-                            placement: "left",
-                        }}
-                    >
+                    <Button variant="secondary" link="/" icon={homeIcon}>
                         Secondary
                     </Button>
                 </ButtonBox>
