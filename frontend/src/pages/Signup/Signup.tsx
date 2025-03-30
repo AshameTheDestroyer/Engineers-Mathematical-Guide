42e146aaaa96d1efe439249aca0cd1519560f3ab
Batoul Kazak <batoulkazak297@gmail.com>
2025-03-30 19:28:43 +0300

    Making Signup Page Responsive

--- frontend/src/pages/Signup/Signup.tsx
+++ frontend/src/pages/Signup/Signup.tsx
@@ -8,7 +8,7 @@ import { Logo } from "@/components/Logo/Logo";
 export default function Signup() {
     return (
         <section className="flex min-h-screen">
-            <div className="flex w-full flex-row">
+            <div className="flex w-full flex-col sm:flex-row">
                 <main className="flex items-center justify-center p-4 md:w-1/2">
                     <form
                         className="flex w-full max-w-md flex-col gap-5 rounded-lg p-6"
@@ -35,11 +35,14 @@ export default function Signup() {
                         />
                         <Button>SignUp</Button>
                         <p>
-                            Already a member? <NavLink>login</NavLink>
+                            Already a member?{" "}
+                            <NavLink className="border-b-1 text-foreground-light">
+                                login
+                            </NavLink>
                         </p>
                     </form>
                 </main>
-                <section className="from-primary-normal to-primary-dark-active relative min-h-screen w-full bg-gradient-to-r md:w-1/2">
+                <section className="from-primary-normal to-primary-dark-active h-30 relative w-full bg-gradient-to-r sm:min-h-screen sm:w-1/3 md:min-h-screen md:w-1/2">
                     <div className="absolute bottom-4 right-4 rounded p-2 text-white">
                         <Logo />
                     </div>
