abc2e01b6afddc10936fb4efe73fe9045b853cb1
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-31 05:37:56 +0300

    Typography Test Page.

--- frontend/src/routes/TestPage.routes.ts
+++ frontend/src/routes/TestPage.routes.ts
@@ -1,5 +1,6 @@
 const LANDING_PAGE_ROUTES_ = [
     { text: "Colours", href: "/test/colours" },
+    { text: "Typography", href: "/test/typography" },
     { text: "Components", href: "/test/components" },
 ];
 
