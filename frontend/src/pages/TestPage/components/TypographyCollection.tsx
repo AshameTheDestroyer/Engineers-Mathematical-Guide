4f99235c3800823297f95727a3b9ca83fbf36b1d
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-31 05:37:34 +0300

    Typography Collection Component.

---
+++ frontend/src/pages/TestPage/components/TypographyCollection.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import { Collection, CollectionProps } from "./Collection";
+import { TypographyDisplay, TypographyDisplayProps } from "./TypographyDisplay";
+import { twMerge } from "tailwind-merge";
+
+export type TypographyCollectionProps = TypographyDisplayProps &
+    Omit<CollectionProps, "children"> & {
+        classNames: Array<string>;
+    };
+
+export const TypographyCollection: FC<TypographyCollectionProps> = ({
+    id,
+    text,
+    title,
+    inner,
+    className,
+    classNames,
+}) => {
+    return (
+        <Collection
+            id={id}
+            className={twMerge(
+                "[&>div]:flex-col [&>div]:flex-nowrap",
+                className
+            )}
+            title={title}
+            inner={inner}
+        >
+            {classNames.map((className, i) => (
+                <TypographyDisplay key={i} className={className} text={text} />
+            ))}
+        </Collection>
+    );
+};
