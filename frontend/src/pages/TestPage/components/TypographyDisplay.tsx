37c33b8c5ba0a41e0b452aee05ec3ccf2e29a2f6
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-03-31 05:28:25 +0300

    Typography Display Component.

---
+++ frontend/src/pages/TestPage/components/TypographyDisplay.tsx
@@ -0,0 +1,42 @@
+import { twJoin, twMerge } from "tailwind-merge";
+import { FC, useContext, useMemo } from "react";
+import { ChildlessComponentProps } from "@/types/ComponentProps";
+import { ThemeContext } from "@/components/ThemeContextProvider/ThemeContextProvider";
+
+export type TypographyDisplayProps = ChildlessComponentProps & {
+    text: string;
+};
+
+export const TypographyDisplay: FC<TypographyDisplayProps> = ({
+    id,
+    text,
+    className,
+}) => {
+    const { isDarkThemed } = useContext(ThemeContext);
+
+    const sizeName = useMemo(
+        () =>
+            className
+                ?.trimAll()
+                .split(" ")
+                .find((token) => token.match(/text-(\d*xl|lg|md|sm|xs)/))
+                ?.replace("text-", ""),
+        [className]
+    );
+
+    return (
+        <div id={id} className={twMerge("flex gap-4", className)}>
+            <span
+                className={twJoin(
+                    !isDarkThemed && "shadow-lg shadow-[#0000004c]",
+                    "bg-secondary-normal min-w-20 place-self-center rounded-2xl py-2 text-center text-lg uppercase"
+                )}
+            >
+                {sizeName}
+            </span>
+            <p className="place-content-center overflow-hidden text-ellipsis whitespace-nowrap [line-height:1.3]">
+                {text}
+            </p>
+        </div>
+    );
+};
