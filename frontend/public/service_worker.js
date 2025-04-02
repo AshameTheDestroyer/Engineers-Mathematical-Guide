38d025ee85c5ccb4e8484807db24bd9b13d02bb6
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-02 21:01:53 +0300

    Better Organized.

--- frontend/public/service_worker.js
+++ frontend/public/service_worker.js
@@ -1,51 +1,48 @@
 const CACHE_KEY = "mathware-engineers-mathematical-guide";
 const URLsToCache = ["/index.html", "/offline.html", "/offline_font.ttf"];
 
-this.addEventListener("install", (e) => {
+this.addEventListener("install", (e) =>
     e.waitUntil(
         caches
             .open(CACHE_KEY)
-            .then((cache) => {
-                console.log("Cache Opened");
-                return cache.addAll(URLsToCache);
-            })
-            .catch((err) => {
-                console.error("Failed to cache resources:", err);
-            })
-    );
-});
-
-this.addEventListener("fetch", (e) => {
-    e.respondWith(
-        caches.match(e.request).then((cachedResponse) => {
-            if (cachedResponse) {
-                return cachedResponse;
-            }
-
-            return fetch(e.request).catch(() => {
-                if (e.request.destination === "font") {
-                    return caches.match("/offline_font.ttf");
-                }
-
-                return caches.match("/offline.html");
-            });
-        })
-    );
-});
-
-this.addEventListener("activate", (e) => {
-    const cacheWhitelist = [CACHE_KEY];
-
-    e.waitUntil(
-        caches.keys().then((cacheKeys) =>
-            Promise.all(
-                cacheKeys.map((cacheKey) => {
-                    if (!cacheWhitelist.includes(cacheKey)) {
-                        console.log(`Deleting old cache: ${cacheKey}`);
-                        return caches.delete(cacheKey);
-                    }
-                })
+            .then(
+                (cache) => (
+                    console.log("Cache Opened"), cache.addAll(URLsToCache)
+                )
             )
-        )
-    );
-});
+            .catch((err) => console.error("Failed to cache resources:", err))
+    )
+);
+
+this.addEventListener("fetch", (e) =>
+    e.respondWith(
+        caches
+            .match(e.request)
+            .then((cachedResponse) =>
+                cachedResponse
+                    ? cachedResponse
+                    : fetch(e.request).catch(() =>
+                          e.request.destination == "font"
+                              ? caches.match("/offline_font.ttf")
+                              : caches.match("/offline.html")
+                      )
+            )
+    )
+);
+
+this.addEventListener("activate", (e) =>
+    e.waitUntil(
+        caches
+            .keys()
+            .then((cacheKeys) =>
+                Promise.all(
+                    cacheKeys.map(
+                        (cacheKey) =>
+                            cacheWhitelist.includes(cacheKey) ||
+                            (console.log(`Deleting old cache: ${cacheKey}`),
+                            caches.delete(cacheKey))
+                    )
+                )
+            )
+    )
+);
