0e77332db6774b5ffa223f9fb467ba0d42f21da7
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-02 18:38:53 +0300

    Consistent Use of Cog Components.

--- frontend/src/pages/LoadingPage/LoadingPage.tsx
+++ frontend/src/pages/LoadingPage/LoadingPage.tsx
@@ -1,8 +1,7 @@
 import { FC } from "react";
 import { Page } from "@components/Page/Page";
-import { Icon } from "@components/Icon/Icon";
-
-import cog_icon from "@icons/cog.svg";
+import { CogIcon } from "@/components/CogIcon/CogIcon";
+import { DoubleCogIcon } from "@/components/DoubleCogIcon/DoubleCogIcon";
 
 export const LoadingPage: FC = () => {
     return (
@@ -10,29 +9,17 @@ export const LoadingPage: FC = () => {
             <div className="centralize bg-gray isolate flex h-32 grow flex-col justify-between gap-5 overflow-hidden">
                 <h1 className="text-3xl font-bold">Loading...</h1>
                 <p>Please wait a moment till page finally loads.</p>
-                <Icon
-                    className="-left-page absolute bottom-0 z-[-1] -translate-x-1/4 translate-y-1/4 animate-spin text-white [animation-duration:60s]"
-                    width={400}
-                    height={400}
-                    source={cog_icon}
+                <DoubleCogIcon
+                    className="-left-page -bottom-1/10 absolute z-[-1] text-white"
+                    size={400}
                 />
-                <Icon
-                    className="-left-page absolute bottom-0 z-[-1] translate-x-[70%] translate-y-[40%] rotate-[-5deg] animate-spin text-white [animation-direction:reverse] [animation-duration:60s]"
-                    width={350}
-                    height={350}
-                    source={cog_icon}
+                <CogIcon
+                    className="-left-page absolute top-0 -translate-y-1/2 translate-x-[10vw] text-white [animation-direction:reverse]"
+                    size={250}
                 />
-                <Icon
-                    className="-right-page absolute top-0 translate-x-1/4 translate-y-2/3 animate-spin text-white [animation-direction:reverse] [animation-duration:60s]"
-                    width={250}
-                    height={250}
-                    source={cog_icon}
-                />
-                <Icon
-                    className="-left-page absolute top-0 -translate-y-1/2 translate-x-[10vw] animate-spin text-white [animation-direction:reverse] [animation-duration:60s]"
-                    width={250}
-                    height={250}
-                    source={cog_icon}
+                <CogIcon
+                    className="-right-page absolute top-0 z-[-1] translate-x-1/4 translate-y-2/3 text-white [animation-direction:reverse]"
+                    size={250}
                 />
             </div>
         </Page>
