ab00884aa917a26645fc62589f491f351261c928
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-02 20:53:56 +0300

    Offline Page Overhauled.

--- frontend/public/service_worker.js
+++ frontend/public/service_worker.js
@@ -1,43 +1,51 @@
 const CACHE_KEY = "mathware-engineers-mathematical-guide";
-const URLsToCache = ["index.html", "offline.html"];
+const URLsToCache = ["/index.html", "/offline.html", "/offline_font.ttf"];
 
 this.addEventListener("install", (e) => {
     e.waitUntil(
         caches
             .open(CACHE_KEY)
-            .then(
-                (cache) => (
-                    console.log("Cache Opened"), cache.addAll(URLsToCache)
-                )
-            )
+            .then((cache) => {
+                console.log("Cache Opened");
+                return cache.addAll(URLsToCache);
+            })
+            .catch((err) => {
+                console.error("Failed to cache resources:", err);
+            })
     );
 });
 
 this.addEventListener("fetch", (e) => {
     e.respondWith(
-        caches
-            .match(e.request)
-            .then(() =>
-                fetch(e.request).catch(() => caches.match(URLsToCache[1]))
-            )
+        caches.match(e.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return fetch(e.request).catch(() => {
+                if (e.request.destination === "font") {
+                    return caches.match("/offline_font.ttf");
+                }
+
+                return caches.match("/offline.html");
+            });
+        })
     );
 });
 
 this.addEventListener("activate", (e) => {
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_KEY);
+    const cacheWhitelist = [CACHE_KEY];
 
     e.waitUntil(
-        caches
-            .keys()
-            .then((cacheKeys) =>
-                Promise.all(
-                    cacheKeys.map(
-                        (cacheKey) =>
-                            cacheWhitelist.includes(cacheKey) ||
-                            caches.delete(cacheKey)
-                    )
-                )
+        caches.keys().then((cacheKeys) =>
+            Promise.all(
+                cacheKeys.map((cacheKey) => {
+                    if (!cacheWhitelist.includes(cacheKey)) {
+                        console.log(`Deleting old cache: ${cacheKey}`);
+                        return caches.delete(cacheKey);
+                    }
+                })
             )
+        )
     );
 });
