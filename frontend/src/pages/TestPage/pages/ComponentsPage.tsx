3f9cb3ed335f10e1ac78bdbb5e7744dfeefbbfd2
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-07 02:59:15 +0300

    Drop Down Collection Component.

--- frontend/src/pages/TestPage/pages/ComponentsPage.tsx
+++ frontend/src/pages/TestPage/pages/ComponentsPage.tsx
@@ -2,11 +2,14 @@ import { FC } from "react";
 import { ButtonCollection } from "../components/ButtonCollection";
 import { IconButtonCollection } from "../components/IconButtonCollection";
 
+import { DropDownCollection } from "../components/DropDownCollection";
+
 export const ComponentsPage: FC = () => {
     return (
         <div className="flex flex-col gap-8">
             <ButtonCollection />
             <IconButtonCollection />
+            <DropDownCollection />
         </div>
     );
 };
