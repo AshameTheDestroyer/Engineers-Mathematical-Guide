35f178dbe5184cad7932596ab93fa98903d64a0e
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-07 02:58:47 +0300

    Partial Type Utility Corrected.

--- frontend/src/types/Partial.d.ts
+++ frontend/src/types/Partial.d.ts
@@ -2,8 +2,11 @@ type PartialDeep<T> = {
     [Key in keyof T]?: T[Key] extends object ? PartialDeep<T[Key]> : T[Key];
 };
 
-type PartialExceptFor<T, TRequiredKeys extends keyof T> = Partial<T> & Pick<T, TRequiredKeys>;
+type PartialExceptFor<T, TRequiredKeys extends keyof T> = Partial<T> &
+    Pick<T, TRequiredKeys>;
 
-type PartialDeepExceptFor<T, TRequiredKeys extends keyof T> = PartialDeep<T> & Pick<T, TRequiredKeys>;
+type PartialDeepExceptFor<T, TRequiredKeys extends keyof T> = PartialDeep<T> &
+    Pick<T, TRequiredKeys>;
 
-type WithPartial<T, Keys extends keyof T> = T | { [Key in Keys]?: T[Key]; };
+type WithPartial<T, Keys extends keyof T> = Omit<T, Keys> &
+    Partial<Pick<T, Keys>>;
