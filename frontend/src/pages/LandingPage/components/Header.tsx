9c0e664aebc1f16cfe85c319cd8bf62a29999ead
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-08 14:51:28 +0300

    Theme Mode Drop Down.

--- frontend/src/pages/LandingPage/components/Header.tsx
+++ frontend/src/pages/LandingPage/components/Header.tsx
@@ -1,14 +1,12 @@
 import { FC } from "react";
 import { Logo } from "@components/Logo/Logo";
 import { Button } from "@components/Button/Button";
-import { DropDown } from "@/components/DropDown/DropDown";
 import { ButtonBox } from "@components/ButtonBox/ButtonBox";
 import { Header as Header_ } from "@components/Header/Header";
 import { LANDING_PAGE_ROUTES } from "@/routes/LandingPage.routes";
-import { DropDownList } from "@/components/DropDownList/DropDownList";
 import { NavigationBar } from "@components/NavigationBar/NavigationBar";
+import { ConfigurationDropDownList } from "./ConfigurationDropDownList";
 
-import cog_icon from "@icons/cog.svg";
 import login_icon from "@icons/login.svg";
 import signup_icon from "@icons/user.svg";
 
@@ -45,32 +43,7 @@ export const Header: FC = () => {
                 >
                     Login
                 </Button>
-                <DropDownList
-                    variant="secondary"
-                    position="bottom-end"
-                    icon={{ source: cog_icon }}
-                >
-                    <DropDown
-                        doesTextGrow
-                        icon={{ placement: "left", className: "-rotate-90" }}
-                        text="Theme Mode"
-                        position="left-start"
-                    >
-                        <Button>Light</Button>
-                        <Button>Dark</Button>
-                        <Button>System</Button>
-                    </DropDown>
-                    <DropDown
-                        doesTextGrow
-                        icon={{ placement: "left", className: "-rotate-90" }}
-                        text="Theme Palette"
-                        position="left-start"
-                    >
-                        <Button>Default</Button>
-                        <Button>Sakura</Button>
-                        <Button>Moss</Button>
-                    </DropDown>
-                </DropDownList>
+                <ConfigurationDropDownList />
             </ButtonBox>
         </Header_>
     );
