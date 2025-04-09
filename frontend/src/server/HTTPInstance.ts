b2cf7d1325dfd151290ec61ba4513604667678bc
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-09 09:30:01 +0300

    Axios HTTP Interceptors Initiated.

---
+++ frontend/src/server/HTTPInstance.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { GetFromLocalStorage } from "@/functions/HandleLocalStorage";
+
+const HTTPInstance = axios.create({
+    timeout: 5000,
+    baseURL: "https://api.example.com",
+    headers: { "Content-Type": "application/json" },
+});
+
+HTTPInstance.interceptors.request.use(
+    (config) => {
+        const token = GetFromLocalStorage("token");
+        if (token != null) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+HTTPInstance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response) {
+            console.error(
+                "Response Error:",
+                error.response.status,
+                error.response.data
+            );
+        } else if (error.request) {
+            console.error("No Response Received:", error.request);
+        } else {
+            console.error("Request Setup Error:", error.message);
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default HTTPInstance;
