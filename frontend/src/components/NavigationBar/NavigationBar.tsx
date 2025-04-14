5918f38dc0c4079fcef408a18e84d155fc886c65
Batoul Kazak <batoulkazak297@gmail.com>
2025-04-14 09:23:17 +0300

    Navigation Back to Normal.

--- frontend/src/components/NavigationBar/NavigationBar.tsx
+++ frontend/src/components/NavigationBar/NavigationBar.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
-// import { Link } from "react-router-dom";
-import { Breadcrumbs } from "../Breadcrumbs/Breadcrumbs";
+import { Link } from "react-router-dom";
 import { ChildlessComponentProps } from "@types_/ComponentProps";
 
 export type NavigationBarProps = ChildlessComponentProps & {
@@ -18,17 +17,11 @@ export const NavigationBar: FC<NavigationBarProps> = ({
     return (
         <nav id={id} className={className}>
             <ul className="flex flex-row flex-wrap place-content-around gap-5">
-                {/* {routes.map((link, i) => (
+                {routes.map((link, i) => (
                     <li key={i}>
                         <Link to={link.href}>{link.text}</Link>
                     </li>
-                ))} */}
-                <Breadcrumbs
-                    anchors={routes}
-                    className="rounded-lg p-4 shadow-sm"
-                    activeItemClasses="text-indigo-700 font-semibold"
-                    inactiveItemClasses="text-gray-500 hover:text-indigo-500"
-                />
+                ))}
             </ul>
         </nav>
     );
