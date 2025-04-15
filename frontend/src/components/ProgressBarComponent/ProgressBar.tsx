16a1cb9f3d562d6d09216f4c8a5c89c1a96f2eef
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-15 18:10:44 +0300

    Checkpoints.

--- frontend/src/components/ProgressBarComponent/ProgressBar.tsx
+++ frontend/src/components/ProgressBarComponent/ProgressBar.tsx
@@ -8,6 +8,7 @@ export type ProgressBarProps = ChildlessComponentProps<HTMLDivElement> & {
     minimum?: number;
     variant?: Variant;
     onComplete?: () => void;
+    checkpoints?: Array<number>;
     onProgress?: (value: number) => void;
 };
 
@@ -21,6 +22,7 @@ export const ProgressBar: FC<ProgressBarProps> = ({
     minimum = 0,
     maximum = 100,
     variant = "primary",
+    checkpoints,
 }) => {
     if (minimum >= maximum) {
         throw new Error("Minimum value must be less than maximum value.");
@@ -32,7 +34,9 @@ export const ProgressBar: FC<ProgressBarProps> = ({
         );
     }
 
-    const percentage = ((value - minimum) / (maximum - minimum)) * 100;
+    function GetPercentage(value: number) {
+        return ((value - minimum) / (maximum - minimum)) * 100;
+    }
 
     useEffect(() => {
         onProgress?.(value);
@@ -44,11 +48,11 @@ export const ProgressBar: FC<ProgressBarProps> = ({
 
     const variantClassNames = {
         default:
-            "[&>div]:bg-tertiary-light [&>div]:border-tertiary-dark border-tertiary-dark bg-tertiary-normal-hover",
+            "[&>div]:bg-tertiary-light [&>div]:border-tertiary-dark border-tertiary-dark bg-tertiary-normal-hover text-tertiary-normal [&>div[data-reached=false]]:bg-tertiary-normal-hover [&>div[data-reached=false]]:text-tertiary-dark",
         primary:
-            "[&>div]:bg-primary-normal [&>div]:border-primary-darker border-primary-darker bg-primary-dark-hover",
+            "[&>div]:bg-primary-normal [&>div]:border-primary-darker border-primary-darker bg-primary-dark-hover text-primary-light [&>div[data-reached=false]]:bg-primary-dark-hover [&>div[data-reached=false]]:text-primary-dark-active",
         secondary:
-            "[&>div]:bg-secondary-normal [&>div]:border-secondary-darker border-secondary-darker bg-secondary-dark-hover",
+            "[&>div]:bg-secondary-normal [&>div]:border-secondary-darker border-secondary-darker bg-secondary-dark-hover text-primary-light [&>div[data-reached=false]]:bg-secondary-dark-hover [&>div[data-reached=false]]:text-secondary-dark-active",
     };
 
     return (
@@ -57,15 +61,33 @@ export const ProgressBar: FC<ProgressBarProps> = ({
             ref={ref}
             className={twMerge(
                 variantClassNames[variant],
-                "border-3 flex h-5 w-full rounded-full",
+                "border-3 relative flex h-5 w-full rounded-full",
                 className
             )}
         >
             <div
                 role="progressbar"
                 className="border-3 -m-[3px] rounded-full transition-all duration-300 ease-out"
-                style={{ width: `${percentage}%` }}
+                style={{ width: `${GetPercentage(value)}%` }}
             />
+            {checkpoints?.map((checkpoint, i) => {
+                if (checkpoint < minimum || checkpoint > maximum) {
+                    throw new Error(
+                        "Checkpoints are out of bounds of minimum value and maximum value."
+                    );
+                }
+
+                return (
+                    <div
+                        key={i}
+                        data-reached={value >= checkpoint}
+                        className="border-3 absolute top-1/2 flex aspect-square w-[calc(3ch+var(--spacing)*4)] -translate-x-[calc(100%-3px)] -translate-y-1/2 place-content-center place-items-center rounded-full font-bold"
+                        style={{ left: `${GetPercentage(checkpoint)}%` }}
+                    >
+                        {checkpoint}
+                    </div>
+                );
+            })}
         </div>
     );
 };
--- frontend/src/pages/TestPage/components/ProgressBarCollection.tsx
+++ frontend/src/pages/TestPage/components/ProgressBarCollection.tsx
@@ -4,10 +4,29 @@ import { ProgressBar } from "@/components/ProgressBarComponent/ProgressBar";
 
 export const ProgressBarCollection: FC = () => {
     return (
-        <Collection title="Progress Bar Component">
-            <ProgressBar variant="default" value={25} />
-            <ProgressBar variant="primary" value={25} />
-            <ProgressBar variant="secondary" value={25} />
+        <Collection title="Progress Bar Component" className="[&>div]:flex-col">
+            <Collection title="Normal" inner className="[&>div]:gap-8">
+                <ProgressBar variant="default" value={25} />
+                <ProgressBar variant="primary" value={25} />
+                <ProgressBar variant="secondary" value={25} />
+            </Collection>
+            <Collection title="Checkpoint-ed" inner className="[&>div]:gap-8">
+                <ProgressBar
+                    value={25}
+                    variant="default"
+                    checkpoints={[25, 50, 75]}
+                />
+                <ProgressBar
+                    value={25}
+                    variant="primary"
+                    checkpoints={[25, 50, 75]}
+                />
+                <ProgressBar
+                    value={25}
+                    variant="secondary"
+                    checkpoints={[25, 50, 75]}
+                />
+            </Collection>
         </Collection>
     );
 };
