f7329fa45f6192080a9282d4b2f80ef59b499071
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-15 19:33:43 +0300

    Locale Component Used.

--- frontend/src/pages/LoadingPage/LoadingPage.tsx
+++ frontend/src/pages/LoadingPage/LoadingPage.tsx
@@ -1,17 +1,16 @@
 import { FC } from "react";
+import { Locale } from "@/components/Locale/Locale";
 import { FallbackPage } from "../FallbackPage/FallbackPage";
-// import locales from "@localization/landing_page.json";
+
 import locals from "@localization/loading_page.json";
-import { useLocalization } from "@/components/LocalizationProvider/LocalizationProvider";
 
 export const LoadingPage: FC = () => {
-    const { GetLocale, language } = useLocalization();
     return (
         <FallbackPage>
-            <h1 className="text-3xl font-bold">
-                {GetLocale(locals.title, language)}
-            </h1>
-            <p>{GetLocale(locals.subtitle, language)}</p>
+            <Locale variant="h1" className="text-3xl font-bold">
+                {locals.title}
+            </Locale>
+            <Locale variant="p">{locals.subtitle}</Locale>
         </FallbackPage>
     );
 };
