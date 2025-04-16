b01b70c0b6d5087fa2ea9c4b246d75ee43c6600e
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-16 15:53:05 +0300

    ID to The Input Element.

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -49,6 +49,7 @@ export const Input: FC<InputProps> = ({
             )}
         >
             <input
+                id={`input-${name}`}
                 className="w-full px-6 py-2"
                 placeholder={placeholder ?? ""}
                 {...props}
