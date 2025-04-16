2ea90e95b9db0ffe1f7e1b1aab1f523d14de2cc6
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-16 21:37:12 +0300

    Responsive Logo & Gradient.

--- frontend/src/pages/RegistrationPage/RegistrationPage.tsx
+++ frontend/src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
-import { twJoin } from "tailwind-merge";
 import { Outlet } from "react-router-dom";
 import { Page } from "@/components/Page/Page";
 import { Logo } from "@/components/Logo/Logo";
+import { twJoin, twMerge } from "tailwind-merge";
 import { useDefaultRoute } from "@/hooks/useDefaultRoute";
 import { useLocalization } from "@/components/LocalizationProvider/LocalizationProvider";
 import { ConfigurationDropDownList } from "@/components/ConfigurationDropDownList/ConfigurationDropDownList";
@@ -24,17 +24,19 @@ export const RegistrationPage: FC = () => {
                 <Outlet />
             </main>
             <section
-                className={twJoin(
+                className={twMerge(
                     direction == "ltr"
                         ? "-mr-page bg-gradient-to-l"
                         : "-ml-page bg-gradient-to-r",
-                    "from-primary-normal -my-page relative h-screen place-self-stretch self-stretch to-transparent max-lg:hidden"
+                    "from-background-dark -my-page relative z-[-1] h-screen place-self-stretch self-stretch to-transparent",
+                    "max-lg:absolute max-lg:inset-0 max-lg:top-3/4 max-lg:m-0 max-lg:h-auto max-lg:bg-gradient-to-t"
                 )}
             >
                 <Logo
                     className={twJoin(
                         direction == "ltr" ? "right-page" : "left-page",
-                        "bottom-page absolute"
+                        "bottom-page absolute max-sm:bottom-[calc(var(--spacing-page)/4)] max-sm:scale-75",
+                        "max-lg:left-1/2 max-lg:-translate-x-1/2"
                     )}
                 />
             </section>
