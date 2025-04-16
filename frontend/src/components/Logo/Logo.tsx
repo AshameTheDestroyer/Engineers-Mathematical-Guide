9095d31b045e99161da41adc041a5fc238aab25c
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-16 20:42:26 +0300

    Generalized Logo.

--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -1,11 +1,20 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
+import { ChildlessComponentProps } from "@/types/ComponentProps";
 
 import logo from "@images/logo_(horizontal).png";
 
-export const Logo: FC = () => {
+export type LogoProps = ChildlessComponentProps<HTMLAnchorElement>;
+
+export const Logo: FC<LogoProps> = ({ id, ref, className }) => {
     return (
-        <Link className="block h-16" to="/">
+        <Link
+            id={id}
+            ref={ref}
+            className={twMerge("block h-16", className)}
+            to="/"
+        >
             <img className="h-full" src={logo} alt="logo" />
         </Link>
     );
