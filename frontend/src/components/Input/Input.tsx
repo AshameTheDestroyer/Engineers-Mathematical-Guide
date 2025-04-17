fe45d23d39f8301de69394a2247d1accae8af399
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-17 17:47:22 +0300

    Input Component Back to Uncontrollable by Default.

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -4,7 +4,6 @@ import { ChildlessComponentProps } from "@/types/ComponentProps";
 import { useLocalization } from "../LocalizationProvider/LocalizationProvider";
 import {
     FC,
-    useState,
     PropsWithChildren,
     InputHTMLAttributes,
     LabelHTMLAttributes,
@@ -23,17 +22,13 @@ export const Input: FC<InputProps> = ({
     name,
     label,
     required,
-    onChange,
     className,
     placeholder,
-    value: _value,
     variant = "default",
     ...props
 }) => {
     const inputID = `input-${name}`;
     const { direction } = useLocalization();
-
-    const [value, setValue] = useState(_value ?? "");
 
     const variantClassNames: VariantClassNames = {
         default: {
@@ -69,10 +64,8 @@ export const Input: FC<InputProps> = ({
             <input
                 id={inputID}
                 className="w-full text-ellipsis rounded-[inherit] px-6 py-2"
-                value={value}
                 required={required}
                 placeholder={placeholder ?? ""}
-                onChange={(e) => (onChange?.(e), setValue(e.target.value))}
                 {...props}
             />
             <Typography<HTMLLabelElement, LabelHTMLAttributes<HTMLLabelElement>>
