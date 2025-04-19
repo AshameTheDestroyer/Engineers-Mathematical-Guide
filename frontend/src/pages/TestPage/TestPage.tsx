eb12d842af0554d45419991e6b944d8745914534
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-19 18:32:57 +0300

    Unused Imports Omitted.

--- frontend/src/pages/TestPage/TestPage.tsx
+++ frontend/src/pages/TestPage/TestPage.tsx
@@ -4,8 +4,6 @@ import { Header } from "./components/Header";
 import { Page } from "@/components/Page/Page";
 import { useDefaultRoute } from "@/hooks/useDefaultRoute";
 import { JumpToTopButton } from "@/components/JumpToTopButton/JumpToTopButton";
-import { Input } from "@/components/Input/Input";
-import { Collection } from "./components/Collection";
 
 export const TestPage: FC = () => {
     useDefaultRoute("/test", "/colours");
