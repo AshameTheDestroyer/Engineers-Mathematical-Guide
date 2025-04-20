52f86acd43305347e4516a9554e18b371ad90e3d
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-20 07:56:02 +0300

    Enum Type Utilities Extended.

--- frontend/src/types/Enum.d.ts
+++ frontend/src/types/Enum.d.ts
@@ -1,1 +1,8 @@
-type Enum<T extends string = string> = readonly [T, ...T[]];
+type EnumValues<T extends string = string> = readonly [T, ...T[]];
+
+type Enum = {
+    readonly [k: string]: string | number;
+    readonly [nu: number]: string;
+};
+
+type ExtractEnumValues<T extends Enum> = EnumValues<T[keyof T]>;
