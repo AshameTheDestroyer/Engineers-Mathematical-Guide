45122549f745019044e60b84f76d4664b0f07cf5
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-30 10:14:43 +0300

    Direction Corrected.

--- frontend/src/pages/RegistrationPage/pages/TermsAndConditionsPage.tsx
+++ frontend/src/pages/RegistrationPage/pages/TermsAndConditionsPage.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import { twJoin } from "tailwind-merge";
 import { Locale } from "@/components/Locale/Locale";
 import { RichText } from "@/components/RichText/RichText";
 import { REGISTRATION_ROUTES } from "@/routes/registration.routes";
@@ -9,7 +10,7 @@ import { useLocalization } from "@/components/LocalizationProvider/LocalizationP
 import locales from "@localization/terms_and_conditions.json";
 
 export const TermsAndConditionsPage: FC = () => {
-    const { GetLocale, language } = useLocalization();
+    const { direction, GetLocale, language } = useLocalization();
 
     const references = GetLocale(
         locales.references as unknown as Record<string, string>,
@@ -18,7 +19,14 @@ export const TermsAndConditionsPage: FC = () => {
 
     return (
         <main className="-mx-6 my-10 flex flex-col gap-8">
-            <main className="flex max-h-[70vh] flex-col gap-8 overflow-y-scroll scroll-smooth px-8 py-4 [&>section:last-child>p]:text-right">
+            <main
+                className={twJoin(
+                    direction == "ltr"
+                        ? "[&>section:last-child>p]:text-left"
+                        : "[&>section:last-child>p]:text-right",
+                    "flex max-h-[70vh] flex-col gap-8 overflow-y-scroll scroll-smooth px-8 py-4"
+                )}
+            >
                 <Locale
                     variant="h1"
                     className="bg-background-light sticky -top-4 z-[1] -mx-8 px-8 py-4 text-2xl font-bold"
