112259280b91ab26b9eca8edf381b48f73678b80
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-04-30 10:39:57 +0300

    Colour Copying Toastified.

--- frontend/src/pages/TestPage/components/ColourDisplay.tsx
+++ frontend/src/pages/TestPage/components/ColourDisplay.tsx
@@ -4,11 +4,15 @@ import { RGBToHex } from "@/functions/RGBToHex";
 import { ChildlessComponentProps } from "@/types/ComponentProps";
 import { FC, useEffect, useMemo, useRef, useState } from "react";
 import { IsDarkColour } from "@/functions/CalculateColourLuminance";
+import { useToast } from "@/components/ToastProvider/ToastProvider";
 import { useThemeMode } from "@/components/ThemeModeProvider/ThemeModeProvider";
+import { useLocalization } from "@/components/LocalizationProvider/LocalizationProvider";
 
 export type ColourDisplayProps = ChildlessComponentProps;
 
 export const ColourDisplay: FC<ColourDisplayProps> = ({ id, className }) => {
+    const { Alert } = useToast();
+    const { language } = useLocalization();
     const { isDarkThemed } = useThemeMode();
     const divReference = useRef<HTMLButtonElement>(null);
 
@@ -47,6 +51,19 @@ export const ColourDisplay: FC<ColourDisplayProps> = ({ id, className }) => {
         });
     }, [isDarkThemed]);
 
+    function OnButtonClick(
+        _e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) {
+        if (colourHex == null) {
+            return;
+        }
+
+        navigator.clipboard.writeText(colourHex?.toLowerCase());
+        Alert(language == "en" ? "Colour copied." : "تَمّ نَسخُ اللّون.", {
+            type: "info",
+        });
+    }
+
     return (
         <button
             id={id}
@@ -58,12 +75,9 @@ export const ColourDisplay: FC<ColourDisplayProps> = ({ id, className }) => {
                 "not-active:[:is(:hover,:focus-within)]:scale-110 shadow-glow flex cursor-pointer flex-col gap-2 rounded-2xl px-6 py-4 text-center font-bold shadow-red-500 transition-[scale] duration-200",
                 className
             )}
+            onClick={OnButtonClick}
             title="Click to copy colour."
             style={{ boxShadow: shadow }}
-            onClick={(_e) =>
-                colourHex != null &&
-                navigator.clipboard.writeText(colourHex?.toLowerCase())
-            }
         >
             <h3 className="grow place-content-center">{colourName}</h3>
             <p className="text-xl">{colourHex != null && colourHex}</p>
