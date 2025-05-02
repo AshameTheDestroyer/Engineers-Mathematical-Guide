5bfcd93dd6892ab1975c22e16633984475eb0bb9
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-02 20:26:04 +0300

    Type Better Written.

--- frontend/src/types/Enum.d.ts
+++ frontend/src/types/Enum.d.ts
@@ -1,7 +1,5 @@
 type EnumValue = string | number;
 type EnumValues = Array<EnumValue>;
+type Enum = Record<string | number, string>;
 type ExtractEnumValue<T extends EnumValue> = `${T}`;
-type Enum = {
-    [Key: string | number]: string;
-};
 type ExtractEnumValues<T extends Enum> = [keyof T, ...(keyof T)[]];
