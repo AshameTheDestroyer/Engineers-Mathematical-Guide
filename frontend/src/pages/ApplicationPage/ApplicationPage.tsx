d9424336956b11611e27f7947b9a1b94c22e854d
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-05 21:16:56 +0300

    Application Route.

---
+++ frontend/src/pages/ApplicationPage/ApplicationPage.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { Page } from "@/components/Page/Page";
+
+export const ApplicationPage: FC = () => {
+    return <Page></Page>;
+};
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,10 +4,12 @@ import { WebsiteRoute } from "./routes/website.routes";
 import { ComposeRoutes } from "./functions/ComposeRoutes";
 import { RegistrationRoute } from "./routes/registration.routes";
 import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
+import { ApplicationRoute } from "./routes/application.routes";
 
 export const Routes = [
     TestRoute,
     WebsiteRoute,
+    ApplicationRoute,
     RegistrationRoute,
     () => <Route path="*" element={<NotFoundPage />} />,
 ];
