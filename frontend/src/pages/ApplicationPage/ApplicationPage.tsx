09c2b9c8114278ac46485e7e485b6f8e0ffcf7c5
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-10 19:31:53 +0300

    Courses Page Initiated.

--- frontend/src/pages/ApplicationPage/ApplicationPage.tsx
+++ frontend/src/pages/ApplicationPage/ApplicationPage.tsx
@@ -1,6 +1,29 @@
 import { FC } from "react";
+import { twJoin } from "tailwind-merge";
+import { Outlet } from "react-router-dom";
 import { Page } from "@/components/Page/Page";
+import { useDefaultRoute } from "@/hooks/useDefaultRoute";
+import { APPLICATION_ROUTES } from "@/routes/application.routes";
+import { useLocalization } from "@/components/LocalizationProvider/LocalizationProvider";
+import { ConfigurationDropDownList } from "@/components/ConfigurationDropDownList/ConfigurationDropDownList";
 
 export const ApplicationPage: FC = () => {
-    return <Page></Page>;
+    useDefaultRoute(
+        APPLICATION_ROUTES.base.href,
+        APPLICATION_ROUTES.base.routes.courses.href
+    );
+
+    const { direction } = useLocalization();
+
+    return (
+        <Page>
+            <ConfigurationDropDownList
+                className={twJoin(
+                    direction == "ltr" ? "right-page" : "left-page",
+                    "top-page absolute z-10"
+                )}
+            />
+            <Outlet />
+        </Page>
+    );
 };
