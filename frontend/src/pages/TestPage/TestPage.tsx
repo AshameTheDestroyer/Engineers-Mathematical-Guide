78eed8837f3cac6cb08d8d11ca861e8599af9b37
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-10 19:43:05 +0300

    Magic Routes Omitted.

--- frontend/src/pages/TestPage/TestPage.tsx
+++ frontend/src/pages/TestPage/TestPage.tsx
@@ -2,11 +2,19 @@ import { FC } from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Page } from "@/components/Page/Page";
+import { TEST_ROUTES } from "@/routes/test.routes";
 import { useDefaultRoute } from "@/hooks/useDefaultRoute";
 import { JumpToTopButton } from "@/components/JumpToTopButton/JumpToTopButton";
 
 export const TestPage: FC = () => {
-    useDefaultRoute("/test", "/colours");
+    if (TEST_ROUTES == null) {
+        return <></>;
+    }
+
+    useDefaultRoute(
+        TEST_ROUTES.base.href,
+        TEST_ROUTES.base.routes.colours.href
+    );
 
     return (
         <Page>
