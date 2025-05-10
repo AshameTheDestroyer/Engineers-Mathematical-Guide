54b6a6f254d0e2f83719cb67d22a5afa03b4ae9c
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-10 19:31:53 +0300

    Courses Page Initiated.

---
+++ frontend/src/pages/ApplicationPage/pages/CoursesPage.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Typography } from "@/components/Typography/Typography";
+
+export const CoursesPage: FC = () => {
+    return (
+        <main>
+            <Typography variant="h1">Courses</Typography>
+        </main>
+    );
+};
