cbaf492c0c8d7694771edf61006f06986225d486
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-11 00:14:46 +0300

    Optional Image.

--- frontend/src/pages/ApplicationPage/pages/CoursesPage.tsx
+++ frontend/src/pages/ApplicationPage/pages/CoursesPage.tsx
@@ -13,20 +13,22 @@ export const CoursesPage: FC = () => {
         requestTime: 0,
     });
 
-    const data = data_.map((datum) =>
-        CourseSchema.parse({ ...datum, image: "" })
-    );
+    const data = data_.map((datum) => CourseSchema.parse(datum));
 
     return (
-        <main>
+        <main className="flex flex-col gap-8">
             <header>
                 <Typography variant="h1" className="text-2xl font-bold">
                     Courses
                 </Typography>
             </header>
-            <main>
+            <main className="grid grid-cols-[repeat(auto-fill,minmax(18rem,1fr))] gap-8">
                 {data.map((datum) => (
-                    <CourseCard key={datum.id} course={datum} />
+                    <CourseCard
+                        key={datum.id}
+                        className="aspect-square"
+                        course={datum}
+                    />
                 ))}
             </main>
         </main>
