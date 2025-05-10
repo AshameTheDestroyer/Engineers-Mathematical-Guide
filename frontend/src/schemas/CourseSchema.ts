3c51f07763c74dd67ab88084fca4e7b185d06bfe
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-11 00:14:46 +0300

    Optional Image.

--- frontend/src/schemas/CourseSchema.ts
+++ frontend/src/schemas/CourseSchema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const CourseSchema = z.object({
+    image: z.string().nullish(),
     id: z.string({ required_error: "required" }),
-    image: z.string({ required_error: "required" }),
     title: z.string({ required_error: "required" }),
     description: z.string({ required_error: "required" }),
     rating: z
