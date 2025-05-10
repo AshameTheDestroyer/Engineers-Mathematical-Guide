f27a80386f59f00a881ff06510dd023ff482028d
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-10 21:35:03 +0300

    Dummy Data Utilization.

--- frontend/src/pages/ApplicationPage/pages/CoursesPage.tsx
+++ frontend/src/pages/ApplicationPage/pages/CoursesPage.tsx
@@ -1,10 +1,26 @@
 import { FC } from "react";
+import { useMockQuery } from "@/hooks/useMockQuery";
+import { CourseCard } from "../components/CourseCard";
 import { Typography } from "@/components/Typography/Typography";
 
+import dummy_data from "./dummy_data.json";
+
 export const CoursesPage: FC = () => {
+    const { data } = useMockQuery({
+        usesSuspense: true,
+        dummyData: dummy_data,
+    });
+
     return (
         <main>
-            <Typography variant="h1">Courses</Typography>
+            <header>
+                <Typography variant="h1" className="text-2xl font-bold">
+                    Courses
+                </Typography>
+            </header>
+            <main>
+                <CourseCard course={data} />
+            </main>
         </main>
     );
 };
