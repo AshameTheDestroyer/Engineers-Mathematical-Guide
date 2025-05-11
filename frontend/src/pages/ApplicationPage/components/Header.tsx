c5148f4c2937d840a661cc60e4feed008f73737b
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-11 05:20:08 +0300

    Header Initiated.

---
+++ frontend/src/pages/ApplicationPage/components/Header.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { Header as Header_ } from "@components/Header/Header";
+import { Breadcrumbs } from "@/components/Breadcrumbs/Breadcrumbs";
+import { ConfigurationDropDownList } from "@/components/ConfigurationDropDownList/ConfigurationDropDownList";
+
+export const Header: FC = () => {
+    return (
+        <Header_
+            isSticky
+            className="bg-background-dark -m-page mb-page px-[calc(var(--spacing)*4)!important] py-2"
+            onScroll={(direction, header) => {
+                header.classList[direction == "up" ? "remove" : "add"](
+                    "not-hover:not-focus-within:opacity-50"
+                );
+            }}
+        >
+            <Breadcrumbs />
+            <ConfigurationDropDownList
+                className="p-0 [&>*>*]:p-1"
+                icon={{ width: 20, height: 20 }}
+            />
+        </Header_>
+    );
+};
