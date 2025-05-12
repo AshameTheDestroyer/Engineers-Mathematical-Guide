fa299ac186db7ae94f8f53388bbdf483b47597a3
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-12 23:23:03 +0300

    Top 10 Grades into Top 10 Students.

--- frontend/src/schemas/CourseSchema.ts
+++ frontend/src/schemas/CourseSchema.ts
@@ -31,12 +31,21 @@ export const DetailedCourseSchema = z.intersection(
             .number({ required_error: "required" })
             .min(10, "minimum")
             .max(90, "maximum"),
-        "top-10-grades": z.array(
-            z
-                .number({ required_error: "required" })
-                .min(0, "minimum")
-                .max(100, "maximum")
-        ),
+        "top-10-students": z
+            .array(
+                z.object({
+                    id: z.string({ required_error: "required" }),
+                    grade: z
+                        .number({ required_error: "required" })
+                        .min(0, "minimum")
+                        .max(100, "maximum"),
+                })
+            )
+            .transform((students) =>
+                students.toSorted(
+                    (student1, student2) => student2.grade - student1.grade
+                )
+            ),
     })
 );
 
