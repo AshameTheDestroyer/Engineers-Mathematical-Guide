21d9327cd98cb2f4a8c243be8364eef0cf060ab3
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-12 21:07:28 +0300

    Course Summary Component.

--- frontend/src/pages/ApplicationPage/components/CourseCard.tsx
+++ frontend/src/pages/ApplicationPage/components/CourseCard.tsx
@@ -1,16 +1,13 @@
 import { FC } from "react";
 import { twMerge } from "tailwind-merge";
 import { useShadow } from "@/hooks/useShadow";
-import { Icon } from "@/components/Icon/Icon";
 import { useNavigate } from "react-router-dom";
+import { CourseSummary } from "./CourseSummary";
 import { Image } from "@/components/Image/Image";
 import { CourseDTO } from "@/schemas/CourseSchema";
-import { Rating } from "@/components/Rating/Rating";
 import { Typography } from "@/components/Typography/Typography";
 import { ChildlessComponentProps } from "@/types/ComponentProps";
 import { APPLICATION_ROUTES } from "@/routes/application.routes";
-
-import user_icon from "@icons/user.svg";
 
 export type CourseCardProps = ChildlessComponentProps<HTMLElement> & {
     course: CourseDTO;
@@ -45,66 +42,9 @@ export const CourseCard: FC<CourseCardProps> = ({
                 )
             }
         >
-            <Typography className="" variant="p">
-                {course.description}
-            </Typography>
+            <Typography variant="p">{course.description}</Typography>
             <figure className="absolute inset-0 z-[-1]">
-                <footer className="absolute inset-8 top-auto z-[1] flex flex-col gap-2 overflow-hidden">
-                    <div className="flex place-items-center gap-2">
-                        <Icon
-                            className="drop-shadow-[3px_3px_1px_#0000007c]"
-                            width={20}
-                            height={20}
-                            source={user_icon}
-                        />
-                        <Typography
-                            className="overflow-hidden text-ellipsis whitespace-nowrap text-nowrap"
-                            variant="p"
-                        >
-                            {Intl.NumberFormat("en-US", {
-                                notation: "compact",
-                                compactDisplay: "short",
-                                maximumFractionDigits: 1,
-                            }).format(course["enrollment-count"]) +
-                                " Enrolled Student"}
-                        </Typography>
-                    </div>
-                    <div className="flex place-items-center gap-2">
-                        <Typography
-                            className="text-vibrant-yellow min-w-[3ch] text-center font-bold"
-                            variant="p"
-                        >
-                            {course.rating}
-                        </Typography>
-                        <Rating
-                            value={course.rating}
-                            iconProps={{
-                                className:
-                                    "drop-shadow-[3px_3px_1px_#0000007c]",
-                                width: 20,
-                                height: 20,
-                                thickness: 0.5,
-                                stroke: "black",
-                            }}
-                        />
-                        <Typography
-                            className="overflow-hidden text-ellipsis whitespace-nowrap text-nowrap"
-                            variant="p"
-                        >
-                            {Intl.NumberFormat("en-US", {
-                                notation: "compact",
-                                compactDisplay: "short",
-                                maximumFractionDigits: 1,
-                            }).format(course["rating-count"]) + " Reviews"}
-                        </Typography>
-                    </div>
-                    <Typography
-                        className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-nowrap text-xl font-bold [text-shadow:2px_2px_5px_black!important]"
-                        variant="figcaption"
-                    >
-                        {course.title}
-                    </Typography>
-                </footer>
+                <CourseSummary course={course} />
                 <Image
                     className="absolute inset-0 [&>img]:h-full [&>img]:object-cover"
                     source={course.image}
