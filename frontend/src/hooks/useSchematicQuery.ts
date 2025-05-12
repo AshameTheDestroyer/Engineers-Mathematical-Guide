5f02cc5a21578e8ed625b5043304b9de94f47a85
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-12 20:33:32 +0300

    useSchematicQuery Custom Hook.

---
+++ frontend/src/hooks/useSchematicQuery.ts
@@ -0,0 +1,74 @@
+import {
+    useExtendedQuery,
+    UseExtendedQueryResult,
+    UseExtendedQueryOptions,
+} from "./useExtendedQuery";
+import { ZodSchema } from "zod";
+import { FieldValues } from "react-hook-form";
+import { QueryClient, QueryKey } from "@tanstack/react-query";
+
+export type UseSchematicQueryResult<
+    TSchema extends FieldValues,
+    TParseFnData = undefined,
+    TUsesSuspense extends boolean = false,
+    TError = Error,
+> = UseExtendedQueryResult<
+    TUsesSuspense,
+    Exclude<
+        TParseFnData extends undefined
+            ? TSchema
+            : Exclude<TParseFnData, undefined>,
+        TParseFnData extends undefined ? never : TSchema
+    >,
+    TError
+>;
+
+export type UseSchematicQueryOptions<
+    TSchema extends FieldValues,
+    TParseFnData = undefined,
+    TUsesSuspense extends boolean = false,
+    TQueryFnData = unknown,
+    TError = Error,
+    TData = TQueryFnData,
+    TQueryKey extends QueryKey = readonly unknown[],
+> = UseExtendedQueryOptions<
+    TUsesSuspense,
+    TQueryFnData,
+    TError,
+    TData,
+    TQueryKey
+> & {
+    schema: ZodSchema<TSchema>;
+    parseFn?: (
+        data: TData | undefined,
+        schema: ZodSchema<TSchema>
+    ) => TParseFnData;
+};
+
+export const useSchematicQuery = <
+    TSchema extends FieldValues,
+    TParseFnData = undefined,
+    TUsesSuspense extends boolean = false,
+    TQueryFnData = unknown,
+    TError = Error,
+    TData = TQueryFnData,
+    TQueryKey extends QueryKey = readonly unknown[],
+>(
+    options: UseSchematicQueryOptions<
+        TSchema,
+        TParseFnData,
+        TUsesSuspense,
+        TQueryFnData,
+        TError,
+        TData,
+        TQueryKey
+    >,
+    queryClient?: QueryClient
+): UseSchematicQueryResult<TSchema, TParseFnData, TUsesSuspense, TError> => {
+    const { data: _data, ...query } = useExtendedQuery(options, queryClient);
+
+    const data =
+        options.parseFn?.(_data, options.schema) ?? options.schema.parse(_data);
+
+    return { data, ...query } as any;
+};
