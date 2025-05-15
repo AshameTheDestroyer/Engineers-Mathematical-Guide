9a4197331b1a7c8e597caed62d97f1d017c31781
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-15 21:57:57 +0300

    Direction-ed Correctly.

--- frontend/src/pages/ApplicationPage/components/RankingBadge.tsx
+++ frontend/src/pages/ApplicationPage/components/RankingBadge.tsx
@@ -48,7 +48,7 @@ export const RankingBadge: FC<RankingBadgeProps> = ({ rank, student }) => {
         ],
     };
 
-    const { isDarkColour, colourHex } = useColour({
+    const { isDarkColour } = useColour({
         updateDelay: 250,
         reference: buttonRef,
         className: classNames.button[rank - 1],
@@ -58,24 +58,29 @@ export const RankingBadge: FC<RankingBadgeProps> = ({ rank, student }) => {
         <button
             ref={buttonRef}
             className={twJoin(
-                direction == "ltr" ? "ml-22" : "",
+                direction == "ltr"
+                    ? "ml-22 max-sm:ml-10"
+                    : "mr-22 max-sm:mr-10",
+                isDarkColour ? "text-white" : "text-black",
                 "bg-background-normal-hover active:bg-background-dark [&:where(:hover,:focus-within)]:bg-background-normal-active relative flex grow cursor-pointer place-content-evenly gap-4 rounded-full p-2 transition duration-200",
                 "[&_.icon]:text-background-normal-hover [&:active_.icon]:text-background-dark [&:where(:hover,:focus-within)_.icon]:text-background-normal-active",
                 classNames.button[rank - 1],
-                classNames.bullet[rank - 1],
-                isDarkColour ? "text-white" : "text-black"
+                classNames.bullet[rank - 1]
             )}
         >
             <div
                 className={twJoin(
                     classNames.bullet[rank - 1],
-                    direction == "ltr" ? "-left-22" : "",
-                    "max-sm:text-md absolute top-1/2 flex aspect-square h-[80%] -translate-y-1/2 translate-x-[10%] place-content-center place-items-center text-lg max-sm:-ml-8 max-sm:-mr-2"
+                    direction == "ltr"
+                        ? "-left-22 translate-x-[10%] max-sm:-left-10 max-sm:-ml-8"
+                        : "-right-22 -translate-x-[10%] max-sm:-right-10 max-sm:-mr-8",
+                    // : "-right-22 -translate-x-[10%] max-sm:-right-12 max-sm:-mr-8",
+                    "max-sm:text-md absolute top-1/2 flex aspect-square h-[80%] -translate-y-1/2 place-content-center place-items-center text-lg"
                 )}
             >
                 {rank <= 3 ? (
                     <Icon
-                        className="border-3 h-full w-full overflow-hidden rounded-full transition duration-200 [&>svg]:h-full [&>svg]:w-full [&>svg]:-translate-y-1"
+                        className="border-3 h-full w-full overflow-hidden rounded-full transition duration-200 max-sm:scale-75 [&>svg]:h-full [&>svg]:w-full [&>svg]:-translate-y-1"
                         thickness={1.5}
                         source={MEDAL_ICONS[rank - 1]}
                     />
@@ -107,7 +112,7 @@ export const RankingBadge: FC<RankingBadgeProps> = ({ rank, student }) => {
             </div>
             <div className="flex flex-col overflow-hidden">
                 <Typography
-                    className="overflow-hidden text-ellipsis whitespace-nowrap text-start text-lg font-bold"
+                    className="max-sm:text-md overflow-hidden text-ellipsis whitespace-nowrap text-start text-lg font-bold"
                     variant="strong"
                 >
                     {`${student.name} ${student.surname}`}
@@ -116,13 +121,15 @@ export const RankingBadge: FC<RankingBadgeProps> = ({ rank, student }) => {
                     className="overflow-hidden text-ellipsis whitespace-nowrap text-start"
                     variant="p"
                 >
-                    @{student.username} {colourHex}
+                    @{student.username}
                 </Typography>
             </div>
             <Typography
                 className={twJoin(
-                    direction == "ltr" ? "ml-auto mr-2" : "ml-2 mr-auto",
-                    "flex aspect-square place-content-center place-items-center text-lg font-bold"
+                    direction == "ltr"
+                        ? "ml-auto mr-2 max-sm:mr-1"
+                        : "ml-2 mr-auto max-sm:ml-1",
+                    "max-sm:text-md flex aspect-square place-content-center place-items-center text-lg font-bold"
                 )}
                 variant="em"
             >
