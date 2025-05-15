451c2487dca01b833532e609dd40d322ad2bb02b
Batoul Kazak <batoulkazak297@gmail.com>
2025-05-15 15:39:42 +0300

    Moving Modal from Courses Page to Test Page

--- frontend/src/pages/TestPage/TestPage.tsx
+++ frontend/src/pages/TestPage/TestPage.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Page } from "@/components/Page/Page";
 import { TEST_ROUTES } from "@/routes/test.routes";
-// import { useDefaultRoute } from "@/hooks/useDefaultRoute";
+import { useDefaultRoute } from "@/hooks/useDefaultRoute";
 import { JumpToTopButton } from "@/components/JumpToTopButton/JumpToTopButton";
 
 export const TestPage: FC = () => {
@@ -11,10 +11,10 @@ export const TestPage: FC = () => {
         return <></>;
     }
 
-    // useDefaultRoute(
-    //     TEST_ROUTES.base.href,
-    //     TEST_ROUTES.base.routes.colours.href
-    // );
+    useDefaultRoute(
+        TEST_ROUTES.base.href,
+        TEST_ROUTES.base.routes.colours.href
+    );
 
     return (
         <Page>
