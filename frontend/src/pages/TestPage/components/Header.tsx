448f2b96cac9c70413541c9c17365800adbfb361
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-16 01:32:14 +0300

    Better Organized.

--- frontend/src/pages/TestPage/components/Header.tsx
+++ frontend/src/pages/TestPage/components/Header.tsx
@@ -1,15 +1,17 @@
 import { FC } from "react";
 import { Logo } from "@/components/Logo/Logo";
-import { Header as Header_ } from "@/components/Header/Header";
-import { ConfigurationDropDownList } from "@/components/ConfigurationDropDownList/ConfigurationDropDownList";
-import { NavigationMenuButton } from "@/components/Drawer/components/NavigationMenuButton";
-import { ButtonBox } from "@/components/ButtonBox/ButtonBox";
-import { useLocalization } from "@/components/LocalizationProvider/LocalizationProvider";
-import route_locales from "@localization/test_page_routes.json";
 import { TEST_ROUTES } from "@/routes/test.routes";
+import { ButtonBox } from "@/components/ButtonBox/ButtonBox";
+import { Header as Header_ } from "@/components/Header/Header";
+import { useLocalization } from "@/components/LocalizationProvider/LocalizationProvider";
+import { NavigationMenuButton } from "@/components/Drawer/components/NavigationMenuButton";
+import { ConfigurationDropDownList } from "@/components/ConfigurationDropDownList/ConfigurationDropDownList";
+
+import route_locales from "@localization/test_page_routes.json";
 
 export const Header: FC = () => {
     const { GetRouteLocales, language } = useLocalization();
+
     return (
         <Header_
             className="bg-background-dark transition duration-200"
