573e8ae51742ab3559eb7978a120a3196d00ed04
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-16 04:50:17 +0300

    Vibrant Progress Bars.

--- frontend/src/pages/TestPage/components/ProgressBarCollection.tsx
+++ frontend/src/pages/TestPage/components/ProgressBarCollection.tsx
@@ -4,8 +4,26 @@ import { ProgressBar } from "@/components/ProgressBar/ProgressBar";
 
 import sun_icon from "@icons/sun.svg";
 import moon_icon from "@icons/moon.svg";
+import epic_icon from "@icons/epic.svg";
+import error_icon from "@icons/error.svg";
+import success_icon from "@icons/success.svg";
+import warning_icon from "@icons/warning.svg";
+import information_icon from "@icons/information.svg";
 
 export const ProgressBarCollection: FC = () => {
+    const themeIconCheckpoints = [
+        { value: 33, icon: { source: sun_icon } },
+        { value: 66, icon: { source: moon_icon } },
+    ];
+
+    const vibrantIconCheckpoints = [
+        { value: 17, icon: { source: information_icon } },
+        { value: 33, icon: { source: warning_icon } },
+        { value: 50, icon: { source: success_icon } },
+        { value: 67, icon: { source: error_icon } },
+        { value: 83, icon: { source: epic_icon } },
+    ];
+
     return (
         <Collection
             className="mb-8 [&>div]:grid [&>div]:grid-cols-[repeat(auto-fill,minmax(40vw,1fr))]"
@@ -70,59 +88,54 @@ export const ProgressBarCollection: FC = () => {
             </Collection>
             <Collection
                 className="[&>div]:gap-8"
-                title="Ranged"
-                typography={{ variant: "h2", className: "text-lg" }}
-            >
-                <ProgressBar
-                    value={375}
-                    minimum={300}
-                    maximum={600}
-                    variant="default"
-                    checkpoints={[300, 600]}
-                />
-                <ProgressBar
-                    value={450}
-                    minimum={300}
-                    maximum={600}
-                    variant="primary"
-                    checkpoints={[300, 600]}
-                />
-                <ProgressBar
-                    value={525}
-                    minimum={300}
-                    maximum={600}
-                    variant="secondary"
-                    checkpoints={[300, 600]}
-                />
-            </Collection>
-            <Collection
-                className="[&>div]:gap-8"
                 title="Icon-ed Checkpoints"
                 typography={{ variant: "h2", className: "text-lg" }}
             >
                 <ProgressBar
                     value={33}
                     variant="default"
-                    checkpoints={[
-                        { value: 33, icon: { source: sun_icon } },
-                        { value: 66, icon: { source: moon_icon } },
-                    ]}
+                    checkpoints={themeIconCheckpoints}
                 />
                 <ProgressBar
                     value={50}
                     variant="primary"
-                    checkpoints={[
-                        { value: 33, icon: { source: sun_icon } },
-                        { value: 66, icon: { source: moon_icon } },
-                    ]}
+                    checkpoints={themeIconCheckpoints}
                 />
                 <ProgressBar
                     value={66}
                     variant="secondary"
-                    checkpoints={[
-                        { value: 33, icon: { source: sun_icon } },
-                        { value: 66, icon: { source: moon_icon } },
-                    ]}
+                    checkpoints={themeIconCheckpoints}
+                />
+            </Collection>
+            <Collection
+                className="[&>div]:gap-8"
+                title="Vibrant"
+                typography={{ variant: "h2", className: "text-lg" }}
+            >
+                <ProgressBar
+                    variant="information"
+                    value={17}
+                    checkpoints={vibrantIconCheckpoints}
+                />
+                <ProgressBar
+                    variant="warning"
+                    value={33}
+                    checkpoints={vibrantIconCheckpoints}
+                />
+                <ProgressBar
+                    variant="success"
+                    value={50}
+                    checkpoints={vibrantIconCheckpoints}
+                />
+                <ProgressBar
+                    variant="error"
+                    value={67}
+                    checkpoints={vibrantIconCheckpoints}
+                />
+                <ProgressBar
+                    variant="epic"
+                    value={83}
+                    checkpoints={vibrantIconCheckpoints}
                 />
             </Collection>
         </Collection>
