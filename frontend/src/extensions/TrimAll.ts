9d152eaf2ea8f1fd340fbb83d113642c3dafb61c
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-17 02:42:45 +0300

    Trim All Better Using RegEx.

--- frontend/src/extensions/TrimAll.ts
+++ frontend/src/extensions/TrimAll.ts
@@ -1,17 +1,8 @@
 interface String {
-
     /** Trims all spaces from the text into one space, (except the first and last one). */
     trimAll(): string;
 }
 
 String.prototype.trimAll = function (): string {
-    let currentValue: string = (this as string).trim(),
-        previousValue: string = currentValue;
-
-    do {
-        previousValue = currentValue;
-        currentValue = currentValue.replaceAll("  ", " ");
-    } while (previousValue != currentValue);
-
-    return currentValue;
-}
+    return this.trim().replace(/\s+/g, " ");
+};
