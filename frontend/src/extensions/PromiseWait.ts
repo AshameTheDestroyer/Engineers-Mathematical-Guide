56662e1d5072413bd690f30c11bb49c911f83ee1
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-17 04:45:29 +0300

    Better Ordered.

--- frontend/src/extensions/PromiseWait.ts
+++ frontend/src/extensions/PromiseWait.ts
@@ -1,8 +1,8 @@
 interface PromiseConstructor {
     /** Creates a Promise that resolves the provided data after waiting for a given time. */
-    wait<T>(time: number, data: T): Promise<T>;
+    wait<T>(data: T, time: number): Promise<T>;
 }
 
-Promise.wait = function <T>(time: number, data: T): Promise<T> {
+Promise.wait = function <T>(data: T, time: number): Promise<T> {
     return new Promise<T>((resolve) => setTimeout(() => resolve(data), time));
 };
