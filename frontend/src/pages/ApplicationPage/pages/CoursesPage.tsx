4cf41cd9cddcdc06036374e7d3e19e372cd9e864
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-17 04:09:37 +0300

    Logic Extracted.

--- frontend/src/pages/ApplicationPage/pages/CoursesPage.tsx
+++ frontend/src/pages/ApplicationPage/pages/CoursesPage.tsx
@@ -1,15 +1,19 @@
 import { z } from "zod";
 import { queryClient } from "@/contexts";
+import { Icon } from "@/components/Icon/Icon";
 import { FC, useEffect, useState } from "react";
 import { Input } from "@/components/Input/Input";
 import { useDebounce } from "@/hooks/useDebounce";
+import { Button } from "@/components/Button/Button";
 import { Flexbox } from "@/components/Flexbox/Flexbox";
+import { RichText } from "@/components/RichText/RichText";
 import { CoursesDisplay } from "../components/CoursesDisplay";
 import { Typography } from "@/components/Typography/Typography";
 import { FilterBySearchQuery } from "../functions/FilterBySearchQuery";
 import { LazyComponent } from "@/components/Lazy/components/LazyComponent";
 import { useSchematicQueryParams } from "@/hooks/useSchematicQueryParams";
 
+import search_off_icon from "@icons/search_off.svg";
 import courses_dummy_data from "../pages/courses.dummy.json";
 
 export const CoursesQueryParamsSchema = z.object({
@@ -79,6 +83,41 @@ export const CoursesPage: FC = () => {
                                     debouncedSearchQuery
                                 )
                             )
+                        }
+                        emptyQueryDisplay={
+                            <Flexbox
+                                gap="4"
+                                className="grow"
+                                direction="column"
+                                placeItems="center"
+                                placeContent="center"
+                            >
+                                <Icon
+                                    source={search_off_icon}
+                                    width={128}
+                                    height={128}
+                                />
+                                <Typography
+                                    className="text-xl font-bold"
+                                    variant="h2"
+                                >
+                                    No Course Found
+                                </Typography>
+                                <RichText
+                                    variant="p"
+                                    className="text-center"
+                                    ExtractedTextRenders={(text) => (
+                                        <span className="text-primary-normal font-bold">
+                                            {text}
+                                        </span>
+                                    )}
+                                >
+                                    {`The term **${debouncedSearchQuery}** was not found, try searching for another thing.`}
+                                </RichText>
+                                <Button onClick={(_e) => setSearchQuery("")}>
+                                    Reset Search
+                                </Button>
+                            </Flexbox>
                         }
                     />
                 </LazyComponent>
