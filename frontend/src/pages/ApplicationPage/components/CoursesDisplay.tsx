46a80105c71a795facfeae628afc4bc63b47bc5b
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-17 03:26:00 +0300

    Logic Extracted.

--- frontend/src/pages/ApplicationPage/components/CoursesDisplay.tsx
+++ frontend/src/pages/ApplicationPage/components/CoursesDisplay.tsx
@@ -1,35 +1,37 @@
 import { FC } from "react";
 import { CourseCard } from "./CourseCard";
+import { QueryFunction } from "@tanstack/react-query";
 import { useExtendedQuery } from "@/hooks/useExtendedQuery";
 import { useSchematicQuery } from "@/hooks/useSchematicQuery";
 import { CourseDTO, CourseSchema } from "@/schemas/CourseSchema";
 
-import courses_dummy_data from "../pages/courses.dummy.json";
-
-export type CoursesDisplayProps = {
-    isSkeleton?: boolean;
-    searchQuery?: string;
-};
+export type CoursesDisplayProps = Either<
+    {
+        isSkeleton: true;
+    },
+    {
+        isSkeleton?: boolean;
+        queryFn: QueryFunction<Array<CourseDTO>>;
+    }
+>;
 
 export const CoursesDisplay: FC<CoursesDisplayProps> = ({
+    queryFn,
     isSkeleton,
-    searchQuery,
 }) => {
     const { data: courses } = useSchematicQuery({
+        queryFn,
         enabled: !isSkeleton,
         schema: CourseSchema,
-        queryKey: ["courses", searchQuery],
-        _optimisticResults: "optimistic",
+        queryKey: ["courses"],
         usesSuspense: !isSkeleton,
-        queryFn: () =>
-            Promise.wait(2000, courses_dummy_data.filter(FilterBySearchQuery)),
         parseFn: (data, schema) =>
             isSkeleton ? [] : data?.map((datum) => schema.parse(datum)),
     });
 
     const { data: images } = useExtendedQuery({
         enabled: !isSkeleton,
-        queryKey: ["courses-images", searchQuery],
+        queryKey: ["courses-images", courses],
         queryFn: () =>
             Promise.all(
                 courses!.map(
@@ -43,24 +45,6 @@ export const CoursesDisplay: FC<CoursesDisplayProps> = ({
                 )
             ).then((entries) => Object.fromEntries(entries)),
     });
-
-    function FilterBySearchQuery(course: CourseDTO) {
-        if (searchQuery == null || searchQuery == "") {
-            return true;
-        }
-
-        const terms = searchQuery.toLowerCase().split(" ");
-        return terms.some(
-            (term) =>
-                course.title.toLowerCase().includes(term) ||
-                course.tags.some((tag) =>
-                    tag
-                        .toLocaleLowerCase()
-                        .split(" ")
-                        .some((word) => word.includes(term))
-                )
-        );
-    }
 
     return (
         <div className="grid grid-cols-[repeat(auto-fill,minmax(18rem,1fr))] gap-8">
