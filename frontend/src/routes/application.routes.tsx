504a1782aeb6ee172e7f011bc66af7a25d476b64
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-21 17:33:08 +0300

    Static Titles.

--- frontend/src/routes/application.routes.tsx
+++ frontend/src/routes/application.routes.tsx
@@ -1,9 +1,9 @@
 import { Route } from "react-router-dom";
+import { Title } from "@/components/Title/Title";
 import { LazyImport } from "@/components/Lazy/Lazy";
 import { BuildRouter } from "@/functions/BuildRouter";
 import { LazyPage } from "@/components/Lazy/components/LazyPage";
 import { LazyComponent } from "@/components/Lazy/components/LazyComponent";
-import { RoutesWithAbsolutePaths } from "@/functions/RoutesWithAbsolutePath";
 
 const CoursePage = LazyImport("./pages/ApplicationPage/pages/CoursePage");
 const CoursesPage = LazyImport("./pages/ApplicationPage/pages/CoursesPage");
@@ -34,6 +34,9 @@ export const ApplicationRoute = () => {
                 element={
                     <LazyComponent>
                         <CoursesPage />
+                        <Title>
+                            {APPLICATION_ROUTES.base.routes.courses.title}
+                        </Title>
                     </LazyComponent>
                 }
             />
