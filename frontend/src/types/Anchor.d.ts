ff74be43192015ff02dd4ed0e2ba9e4552e7b538
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-21 17:00:15 +0300

    Typed Correctly.

--- frontend/src/types/Anchor.d.ts
+++ frontend/src/types/Anchor.d.ts
@@ -1,5 +1,21 @@
 type Anchor = {
-    text?: string;
     href: string;
+    title?: string;
     routes?: Record<string, Anchor>;
+};
+
+type AnchorWithAbsolutePath<T extends Anchor> = Omit<T, "routes"> & {
+    absolute: string;
+} & (T["routes"] extends {}
+        ? {
+              routes: {
+                  [K in keyof T["routes"]]: AnchorWithAbsolutePath<
+                      (T["routes"] & {})[K]
+                  >;
+              };
+          }
+        : {});
+
+type AbsoluteAnchor<T extends Record<string, Anchor>> = {
+    [K in keyof T]: AnchorWithAbsolutePath<T[K]>;
 };
