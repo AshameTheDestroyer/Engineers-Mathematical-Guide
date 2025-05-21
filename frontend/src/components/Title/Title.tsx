901c3a0f5f6ff94125d6f520b3781bb968ce4a44
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-21 18:30:22 +0300

    Exact & Root Title.

--- frontend/src/components/Title/Title.tsx
+++ frontend/src/components/Title/Title.tsx
@@ -1,12 +1,19 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 
 export type TitleProps = {
+    exact?: boolean;
     children: string;
 };
 
-export const Title: FC<TitleProps> = ({ children }) => {
+export const Title: FC<TitleProps> = ({ exact, children }) => {
+    const [rootTitle, _setRootTitle] = useState(document.title);
+
     useEffect(() => {
-        document.title = children;
+        document.title = exact ? children : `${rootTitle} - ${children}`;
+
+        return () => {
+            document.title = rootTitle;
+        };
     }, [children]);
 
     return <></>;
