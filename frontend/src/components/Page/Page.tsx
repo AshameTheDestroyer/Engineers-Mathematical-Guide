a9fa52e6f010e535859fe8a3acd46b1c619a63cd
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-21 17:31:31 +0300

    Reference Added.

--- frontend/src/components/Page/Page.tsx
+++ frontend/src/components/Page/Page.tsx
@@ -2,12 +2,13 @@ import { FC } from "react";
 import { twMerge } from "tailwind-merge";
 import { ComponentProps } from "@types_/ComponentProps";
 
-export type PageProps = ComponentProps;
+export type PageProps = ComponentProps<HTMLDivElement>;
 
-export const Page: FC<PageProps> = ({ id, className, children }) => {
+export const Page: FC<PageProps> = ({ id, ref, className, children }) => {
     return (
         <main
             id={id}
+            ref={ref}
             className={twMerge(
                 "page p-page relative isolate flex min-h-screen flex-col transition duration-200",
                 className
