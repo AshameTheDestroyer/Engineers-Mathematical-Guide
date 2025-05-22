7456556e9d572455edc838740bf374e7ba39f35f
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-23 01:09:59 +0300

    Logic Simplified.

--- frontend/src/pages/ApplicationPage/pages/CoursesPage.tsx
+++ frontend/src/pages/ApplicationPage/pages/CoursesPage.tsx
@@ -1,18 +1,14 @@
 import { z } from "zod";
 import { queryClient } from "@/contexts";
-import { Icon } from "@/components/Icon/Icon";
 import { FC, useEffect, useState } from "react";
 import { Input } from "@/components/Input/Input";
 import { useDebounce } from "@/hooks/useDebounce";
-import { Button } from "@/components/Button/Button";
 import { Flexbox } from "@/components/Flexbox/Flexbox";
-import { RichText } from "@/components/RichText/RichText";
 import { CoursesDisplay } from "../components/CoursesDisplay";
 import { Typography } from "@/components/Typography/Typography";
-import { LazyComponent } from "@/components/Lazy/components/LazyComponent";
+import { useGetCourses } from "@/services/Courses/useGetCourses";
 import { useSchematicQueryParams } from "@/hooks/useSchematicQueryParams";
-
-import search_off_icon from "@icons/search_off.svg";
+import { SearchResultDisplay } from "@/components/SearchResultDisplay/SearchResultDisplay";
 
 export const CoursesQueryParamsSchema = z.object({
     query: z.string().optional().default(""),
@@ -25,6 +21,15 @@ export const CoursesPage: FC = () => {
 
     const [searchQuery, setSearchQuery] = useState(queryParams?.query ?? "");
     const debouncedSearchQuery = useDebounce(searchQuery);
+
+    const {
+        refetch,
+        isError,
+        isLoading,
+        data: courses,
+    } = useGetCourses(debouncedSearchQuery);
+
+    const skeletonCourses = new Array(20).fill(null);
 
     useEffect(() => {
         setQueryParams((queryParams) => ({
@@ -60,46 +65,31 @@ export const CoursesPage: FC = () => {
                 />
             </Flexbox>
             <Flexbox className="grow" variant="main">
-                <LazyComponent skeleton={<CoursesDisplay isSkeleton />}>
-                    <CoursesDisplay
+                {isLoading || courses == null ? (
+                    <CoursesDisplay isSkeleton courses={skeletonCourses} />
+                ) : isError ? (
+                    <SearchResultDisplay
+                        state="error"
+                        title="Error!"
+                        Refetch={refetch}
+                        resetButtonText="Refetch Courses"
                         searchQuery={debouncedSearchQuery}
-                        emptyQueryDisplay={
-                            <Flexbox
-                                gap="4"
-                                className="grow"
-                                direction="column"
-                                placeItems="center"
-                                placeContent="center"
-                            >
-                                <Icon
-                                    source={search_off_icon}
-                                    width={128}
-                                    height={128}
-                                />
-                                <Typography
-                                    className="text-xl font-bold"
-                                    variant="h2"
-                                >
-                                    No Course Found
-                                </Typography>
-                                <RichText
-                                    variant="p"
-                                    className="text-center"
-                                    ExtractedTextRenders={(text) => (
-                                        <span className="text-primary-normal font-bold">
-                                            {text}
-                                        </span>
-                                    )}
-                                >
-                                    {`The term **${debouncedSearchQuery}** was not found, try searching for another thing.`}
-                                </RichText>
-                                <Button onClick={(_e) => setSearchQuery("")}>
-                                    Reset Search
-                                </Button>
-                            </Flexbox>
+                        paragraph="An error occurred while fetching courses, try refetching."
+                    />
+                ) : courses.length == 0 ? (
+                    <SearchResultDisplay
+                        state="not-found"
+                        title="No Courses Found"
+                        resetButtonText="Clear Search"
+                        setSearchQuery={setSearchQuery}
+                        searchQuery={debouncedSearchQuery}
+                        paragraph={(searchQuery) =>
+                            `The term **${searchQuery}** was not found, try searching for another thing.`
                         }
                     />
-                </LazyComponent>
+                ) : (
+                    <CoursesDisplay courses={courses} />
+                )}
             </Flexbox>
         </Flexbox>
     );
