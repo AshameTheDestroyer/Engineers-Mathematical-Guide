ffd530b386941a87f02541609e506a5525adf511
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-23 01:08:02 +0300

    useGetCourseByID Service Initialized.

---
+++ frontend/src/services/Courses/useGetCourseByID.ts
@@ -0,0 +1,31 @@
+import {
+    DetailedCourseDTO,
+    DetailedCourseSchema,
+} from "@/schemas/CourseSchema";
+import {
+    useSchematicQuery,
+    InheritableQueryOptions,
+} from "@/hooks/useSchematicQuery";
+
+import detailed_courses_dummy_data from "@data/detailed_courses.dummy.json";
+
+export const useGetCourseByID = <TUsesSuspense extends boolean = false>(
+    id: string | undefined,
+    options?: InheritableQueryOptions<
+        TUsesSuspense,
+        DetailedCourseDTO,
+        DetailedCourseDTO | undefined
+    >
+) =>
+    useSchematicQuery<
+        TUsesSuspense,
+        DetailedCourseDTO,
+        DetailedCourseDTO | undefined
+    >({
+        schema: DetailedCourseSchema,
+        queryFn: () =>
+            detailed_courses_dummy_data.find((course) => course.id == id),
+        parseFn: (data, schema) => (data != null ? schema.parse(data) : data),
+        ...options,
+        queryKey: ["course", ...(options?.queryKey ?? [])],
+    });
