95c161502f2512de0d02fd950b614447d8b99aeb
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-05-23 23:57:55 +0300

    Detailed & Non-Detailed Data Separated.

--- frontend/src/schemas/LearningTracksSchema.ts
+++ frontend/src/schemas/LearningTracksSchema.ts
@@ -1,40 +1,41 @@
 import { z } from "zod";
 
-export const DetailedLearningTrackSchema = z.object({
-    id: z.string({ required_error: "ID is required" }),
-    image: z.string().optional(), // Optional unless specified
-    title: z.string({ required_error: "Title is required" }),
-    description: z.string({ required_error: "Description is required" }),
-    "detailed-description": z.string({
-        required_error: "Detailed description is required",
-    }),
+export const LearningTrackSchema = z.object({
+    id: z.string({ required_error: "required" }),
+    image: z.string().optional(),
+    title: z.string({ required_error: "required" }),
+    description: z.string({ required_error: "required" }),
     rating: z
-        .number({ required_error: "Rating is required" })
-        .min(0, { message: "Rating must be at least 0" })
-        .max(5, { message: "Rating must be at most 5" }),
+        .number({ required_error: "required" })
+        .min(0, { message: "minimum" })
+        .max(5, { message: "maximum" }),
     "rating-count": z
-        .number({ required_error: "Rating count is required" })
-        .int()
-        .nonnegative({
-            message: "Rating count must be a non-negative integer",
-        }),
+        .number({ required_error: "required" })
+        .nonnegative("nonnegative"),
     "courses-count": z
-        .number({ required_error: "Courses count is required" })
-        .int()
-        .nonnegative({
-            message: "Courses count must be a non-negative integer",
-        }),
+        .number({ required_error: "required" })
+        .nonnegative("nonnegative"),
     "specialized-count": z
-        .number({ required_error: "Specialized count is required" })
-        .int()
-        .nonnegative({
-            message: "Specialized count must be a non-negative integer",
-        }),
-    tags: z.array(z.string({ required_error: "Each tag must be a string" }), {
-        required_error: "Tags are required",
+        .number({ required_error: "required" })
+        .nonnegative("nonnegative"),
+    tags: z.array(z.string({ required_error: "required" }), {
+        required_error: "required",
     }),
-    courses: z.array(
-        z.string({ required_error: "Each course must be a string" }),
-        { required_error: "Courses are required" }
-    ),
 });
+
+export const DetailedLearningTrackSchema = z.intersection(
+    LearningTrackSchema,
+    z.object({
+        "detailed-description": z.string({
+            required_error: "required",
+        }),
+        courses: z.array(z.string({ required_error: "required" }), {
+            required_error: "required",
+        }),
+    })
+);
+
+export type LearningTrackDTO = z.infer<typeof LearningTrackSchema>;
+export type DetailedLearningTrackDTO = z.infer<
+    typeof DetailedLearningTrackSchema
+>;
