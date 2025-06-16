d610b0daea5c2af3e9b0e61f78b5892d8b7cbe07
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-06-16 23:19:59 +0300

    useGetSimilarLearningTracks.

---
+++ frontend/src/services/LearningTracks/useGetSimilarLearningTracks.ts
@@ -0,0 +1,37 @@
+import { useGetLearningTracks } from "./useGetLearningTracks";
+import { LearningTrackDTO } from "@/schemas/LearningTrackSchema";
+import { InheritableQueryOptions } from "@/hooks/useSchematicQuery";
+
+export const SIMILAR_LEARNING_TRACKS_LIMIT = 5;
+export const GET_SIMILAR_LEARNING_TRACKS_KEY = "get-similar-learning-tracks";
+
+export const useGetSimilarLearningTracks = <
+    TUsesSuspense extends boolean = false,
+>(
+    learningTrack: LearningTrackDTO,
+    limit: number = SIMILAR_LEARNING_TRACKS_LIMIT,
+    options?: InheritableQueryOptions<
+        TUsesSuspense,
+        LearningTrackDTO,
+        Array<LearningTrackDTO>
+    >
+) => {
+    const { data: learningTracks, ...result } = useGetLearningTracks(
+        learningTrack.tags.join(" "),
+        {
+            ...(options ?? ({} as typeof options & {})),
+            queryKey: [
+                GET_SIMILAR_LEARNING_TRACKS_KEY,
+                learningTrack.id,
+                ...(options?.queryKey ?? []),
+            ],
+        }
+    );
+
+    return {
+        data: learningTracks
+            ?.filter((learningTrack_) => learningTrack_.id != learningTrack.id)
+            ?.slice(0, limit),
+        ...result,
+    };
+};
