b6bb87b4d1c6ac0a3ef789182b237908b30c82c7
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-06-29 01:32:17 +0300

    Math Equations Page Initiated.

--- frontend/src/pages/ApplicationPage/pages/MathEquationsPage.tsx
+++ frontend/src/pages/ApplicationPage/pages/MathEquationsPage.tsx
@@ -1,8 +1,31 @@
+import { z } from "zod";
 import { FC } from "react";
+import { twJoin } from "tailwind-merge";
+import { Button } from "@/components/Button/Button";
 import { Flexbox } from "@/components/Flexbox/Flexbox";
+import { useSchematicQueryParams } from "@/hooks/useSchematicQueryParams";
 import { MathParallaxScene } from "@/components/MathParallaxScene/MathParallaxScene";
 
+export enum MathEquationsModeEnum {
+    cards = "cards",
+    parallax = "parallax",
+}
+
+export type MathEquationsMode = ExtractEnumValue<MathEquationsModeEnum>;
+
+export const MathEquationsQueryParamsSchema = z.object({
+    query: z.string().optional().default(""),
+    mode: z
+        .nativeEnum(MathEquationsModeEnum)
+        .optional()
+        .default(MathEquationsModeEnum.parallax),
+});
+
 export const MathEquationsPage: FC = () => {
+    const { queryParams, setQueryParams } = useSchematicQueryParams(
+        MathEquationsQueryParamsSchema
+    );
+
     return (
         <Flexbox
             gap="8"
@@ -10,7 +33,30 @@ export const MathEquationsPage: FC = () => {
             direction="column"
             className="flex-grow"
         >
-            <MathParallaxScene className="flex-grow" />
+            <MathParallaxScene
+                className={twJoin(
+                    "flex-grow duration-300",
+                    queryParams.mode != MathEquationsModeEnum.parallax &&
+                        "pointer-events-none opacity-0"
+                )}
+            />
+
+            <Button
+                className="mx-auto w-full max-w-64"
+                onClick={(_e) =>
+                    setQueryParams((queryParams) => ({
+                        ...queryParams,
+                        mode:
+                            queryParams?.mode == MathEquationsModeEnum.parallax
+                                ? MathEquationsModeEnum.cards
+                                : MathEquationsModeEnum.parallax,
+                    }))
+                }
+            >
+                {queryParams.mode == MathEquationsModeEnum.parallax
+                    ? "Show Cards"
+                    : "Toggle Parallax"}
+            </Button>
         </Flexbox>
     );
 };
