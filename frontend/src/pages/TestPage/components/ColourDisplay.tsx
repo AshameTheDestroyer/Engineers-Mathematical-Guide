824ea54365255066e6b04342d5a4f8acba45d494
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-07-02 23:35:20 +0300

    useClipboard Custom Hook Initiated.

--- frontend/src/pages/TestPage/components/ColourDisplay.tsx
+++ frontend/src/pages/TestPage/components/ColourDisplay.tsx
@@ -1,10 +1,9 @@
 import { twMerge } from "tailwind-merge";
 import { useShadow } from "@/hooks/useShadow";
 import { useColour } from "@/hooks/useColour";
+import { useClipboard } from "@/hooks/useClipboard";
 import { FC, useImperativeHandle, useRef } from "react";
 import { ChildlessComponentProps } from "@/types/ComponentProps";
-import { useToast } from "@/components/ToastProvider/ToastProvider";
-import { useLocalization } from "@/components/LocalizationProvider/LocalizationProvider";
 
 export type ColourDisplayProps = ChildlessComponentProps<HTMLButtonElement>;
 
@@ -13,8 +12,11 @@ export const ColourDisplay: FC<ColourDisplayProps> = ({
     ref,
     className,
 }) => {
-    const { Alert } = useToast();
-    const { language } = useLocalization();
+    const { CopyToClipboard } = useClipboard({
+        ar: "تَمّ نَسخُ اللّون.",
+        en: "Colour copied.",
+    });
+
     const buttonReference = useRef<HTMLButtonElement>(null);
 
     useImperativeHandle(ref, () => buttonReference.current!);
@@ -28,19 +30,6 @@ export const ColourDisplay: FC<ColourDisplayProps> = ({
         `var(--color-${colourName?.replaceAll(" ", "-").toLocaleLowerCase()})`
     );
 
-    function OnButtonClick(
-        _e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-    ) {
-        if (colourHex == null) {
-            return;
-        }
-
-        navigator.clipboard.writeText(colourHex?.toLowerCase());
-        Alert(language == "en" ? "Colour copied." : "تَمّ نَسخُ اللّون.", {
-            type: "info",
-        });
-    }
-
     return (
         <button
             id={id}
@@ -50,9 +39,9 @@ export const ColourDisplay: FC<ColourDisplayProps> = ({
                 "not-active:[:is(:hover,:focus-within)]:scale-110 shadow-glow flex cursor-pointer flex-col gap-2 rounded-2xl px-6 py-4 text-center font-bold shadow-red-500 transition-[scale] duration-200",
                 className
             )}
-            onClick={OnButtonClick}
             title="Click to copy colour."
             style={{ boxShadow: shadow }}
+            onClick={(_e) => CopyToClipboard(colourHex?.toLowerCase())}
         >
             <h3 className="grow place-content-center">{colourName}</h3>
             <p className="text-xl">{colourHex != null && colourHex}</p>
