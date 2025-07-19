17c9496cd7678d87873ac64a873bac174462d740
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-07-20 01:02:05 +0300

    useGetUsersByIDs Services Initiated.

---
+++ frontend/src/services/Users/useGetFollowees.ts
@@ -0,0 +1,24 @@
+import { QueryClient } from "@tanstack/react-query";
+import { useGetUsersByIDs } from "./useGetUsersByIDs";
+import { UserDTO, DetailedUserDTO } from "@/schemas/UserSchema";
+import { InheritableQueryOptions } from "@/hooks/useSchematicQuery";
+
+export const GET_FOLLOWEES_KEY = "get-followees";
+
+export const useGetFollowees = <TUsesSuspense extends boolean = false>(
+    user: DetailedUserDTO,
+    options?: InheritableQueryOptions<TUsesSuspense, UserDTO, Array<UserDTO>>,
+    queryClient?: QueryClient
+) =>
+    useGetUsersByIDs(
+        user.followees,
+        {
+            ...(options ?? ({} as typeof options & {})),
+            queryKey: [
+                GET_FOLLOWEES_KEY,
+                user.username,
+                ...(options?.queryKey ?? []),
+            ],
+        },
+        queryClient
+    );
---
+++ frontend/src/services/Users/useGetFollowers.ts
@@ -0,0 +1,24 @@
+import { QueryClient } from "@tanstack/react-query";
+import { useGetUsersByIDs } from "./useGetUsersByIDs";
+import { UserDTO, DetailedUserDTO } from "@/schemas/UserSchema";
+import { InheritableQueryOptions } from "@/hooks/useSchematicQuery";
+
+export const GET_FOLLOWERS_KEY = "get-followers";
+
+export const useGetFollowers = <TUsesSuspense extends boolean = false>(
+    user: DetailedUserDTO,
+    options?: InheritableQueryOptions<TUsesSuspense, UserDTO, Array<UserDTO>>,
+    queryClient?: QueryClient
+) =>
+    useGetUsersByIDs(
+        user.followers,
+        {
+            ...(options ?? ({} as typeof options & {})),
+            queryKey: [
+                GET_FOLLOWERS_KEY,
+                user.username,
+                ...(options?.queryKey ?? []),
+            ],
+        },
+        queryClient
+    );
