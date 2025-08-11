a8e99f71855e1fc871811c75baf0e0366f0a5c37
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-08-11 21:39:10 +0300

    useGetLessons Service Initiated.

--- frontend/src/pages/DiscoverPage/pages/ModulePage.tsx
+++ frontend/src/pages/DiscoverPage/pages/ModulePage.tsx
@@ -1,6 +1,22 @@
 import { FC } from "react";
+import { useParams } from "react-router-dom";
 import { Flexbox } from "@/components/Flexbox/Flexbox";
+import { DISCOVER_ROUTES } from "@/routes/discover.routes";
+import { useGetLessons } from "@/services/Lessons/useGetLessons";
 
 export const ModulePage: FC = () => {
-    return <Flexbox></Flexbox>;
+    const { courseID, moduleID } =
+        useParams<keyof typeof DISCOVER_ROUTES.base.routes>();
+
+    const { data: lessons } = useGetLessons(courseID, moduleID, {
+        usesSuspense: true,
+    });
+
+    return (
+        <Flexbox direction="column" gap="4">
+            {lessons.map((lesson, i) => (
+                <p key={i}>{lesson.title}</p>
+            ))}
+        </Flexbox>
+    );
 };
