f794c8c828c5f044993c58a3e3da0d1f1158f821
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-08-13 01:52:36 +0300

    Lesson Page Initiated 2.

--- frontend/src/pages/DiscoverPage/pages/LessonPage.tsx
+++ frontend/src/pages/DiscoverPage/pages/LessonPage.tsx
@@ -1,10 +1,18 @@
 import { FC } from "react";
 import { useParams } from "react-router-dom";
+import { Title } from "@/components/Title/Title";
 import { Flexbox } from "@/components/Flexbox/Flexbox";
 import { DISCOVER_ROUTES } from "@/routes/discover.routes";
+import { Typography } from "@/components/Typography/Typography";
 import { useGetLessonByID } from "@/services/Lessons/useGetLessonByID";
+import { useLocalization } from "@/components/LocalizationProvider/LocalizationProvider";
+import { SearchResultDisplay } from "@/components/SearchResultDisplay/SearchResultDisplay";
+
+import locales from "@localization/modules_page.json";
 
 export const LessonPage: FC = () => {
+    const { language, GetLocale } = useLocalization();
+
     const { courseID, moduleID, lessonID } =
         useParams<keyof typeof DISCOVER_ROUTES.base.routes>();
 
@@ -12,5 +20,40 @@ export const LessonPage: FC = () => {
         usesSuspense: true,
     });
 
-    return <Flexbox>{lesson?.title ?? "d"}</Flexbox>;
+    if (courseID == null || moduleID == null) {
+        return (
+            <SearchResultDisplay
+                className="grow"
+                iconType="empty"
+                title={GetLocale(locales.display["empty"].title, language)}
+                paragraph={GetLocale(
+                    locales.display["empty"].paragraph,
+                    language
+                ).replace(/\*\*([^\*]+)\*\*/, `**"${courseID}"**`)}
+            />
+        );
+    }
+
+    if (lesson == null) {
+        return (
+            <SearchResultDisplay
+                className="grow"
+                iconType="empty"
+                title={GetLocale(locales.lessons["empty"].title, language)}
+                paragraph={GetLocale(
+                    locales.lessons["empty"].paragraph,
+                    language
+                ).replace(/\*\*([^\*]+)\*\*/, `**"${moduleID}"**`)}
+            />
+        );
+    }
+
+    return (
+        <Flexbox>
+            <Title>{lesson.title}</Title>
+            <Typography className="text-xl font-bold" variant="h1">
+                {lesson.title}
+            </Typography>
+        </Flexbox>
+    );
 };
