460912229f7caff59d0afb0f3b2c829d037dcc6b
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-08-24 05:54:42 +0300

    Rebuilt.

--- assets/AboutPage-hny-dyZc.js
+++
@@ -1,1 +0,0 @@
-import{j as o,F as r}from"./index-UR4dbTZR.js";const a=()=>o.jsx(r,{className:"grow",variant:"main",direction:"column",gap:"8",children:"About"});export{a as AboutPage};
---
+++ assets/useGetCoursesByIDs-TXCkCRH2.js
@@ -0,0 +1,1 @@
+import{C as y}from"./CourseSchema-D8G-WHV5.js";import{o as _}from"./index-BRPsueAE.js";import{c as a}from"./courses.dummy-zCi7JDTP.js";const f="get-courses-by-ids",h=(m,r,u)=>_({schema:y,queryFn:()=>m.map(e=>a.find(s=>s.id==e)).filter(e=>e!=null),parseFn:(e,s)=>(e==null?void 0:e.map(c=>s.parse(c)))??[],...r,queryKey:[f,...m,...(r==null?void 0:r.queryKey)??[]]},u);export{h as u};
