0a8c1c2444c239a2dbd3720c1f8a13765ab543f2
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-08-25 18:28:18 +0300

    Rebuilt.

---
+++ assets/index-Bm9ODkEh.js
@@ -0,0 +1,1 @@
+import{a4 as r}from"./index-DNwHBppe.js";var o=r();export{o as r};
--- assets/useScrollRestoration-D5STGzW8.js
+++
@@ -1,1 +0,0 @@
-import{X as t,r as s}from"./index-nnsqk7fk.js";const e=()=>{const{pathname:o}=t();s.useEffect(()=>{window.scrollTo({top:0,left:0,behavior:"instant"})},[o])};export{e as u};
