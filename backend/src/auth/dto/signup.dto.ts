6ec013729a35b317a0d0be10aa6745b346940acb
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-09-11 18:02:47 +0300

    Gender Added.

--- backend/src/auth/dto/signup.dto.ts
+++ backend/src/auth/dto/signup.dto.ts
@@ -1,52 +1,53 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength, ValidateNested } from 'class-validator';
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength, ValidateNested } from 'class-validator';
 import { LocalString } from 'src/utils/local-string';
+import { UserGender } from "src/utils/types";
 
 export class SignUpDTO {
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
   username: string;
-  
+
   @IsEmail()
   @IsNotEmpty()
   @ApiProperty()
   email: string;
-  
+
   @IsString()
   @MinLength(8)
   @ApiProperty()
   password: string;
-  
+
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
   phoneNumber: string;
-  
+
   @ValidateNested()
   @Type(() => LocalString)
   @ApiProperty({example:{"ar":"مرحبا","en":"Hello"}})
   name: LocalString;
-  
+
   @ValidateNested()
   @Type(() => LocalString)
   @ApiProperty({example:{"ar":"مرحبا","en":"Hello"}})
   surname: LocalString;
-  
+
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
   country: string;
-  
+
   @ValidateNested()
   @Type(() => LocalString)
   @ApiProperty({example:{"ar":"مرحبا","en":"Hello"}})
   bio: LocalString;
 
   @IsOptional()
-  @ApiProperty({ 
-    type: 'string', 
+  @ApiProperty({
+    type: 'string',
     format: 'binary',
     description: 'Profile image file (optional)',
     required: false
@@ -54,15 +55,23 @@ export class SignUpDTO {
   profileImage: any;
 
   @IsOptional()
-  @ApiProperty({ 
-    type: 'string', 
+  @ApiProperty({
+    type: 'string',
     format: 'binary',
     description: 'Cover image file (optional)',
     required: false
   })
   coverImage: any;
 
+    @ApiProperty({
+        enum: UserGender,
+        example: UserGender.MALE
+    })
+    @IsEnum(UserGender, { message: 'Invalid UserGender male OR female' })
+    @IsNotEmpty({ message: 'UserGender is required' })
+    type: UserGender
+
   followersCount: number = 0
-  
+
   followeeCount: number = 0
 }
